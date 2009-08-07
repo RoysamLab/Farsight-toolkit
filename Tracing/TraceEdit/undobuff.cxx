de86bbd536854d781722f01027df54f8f0ce103c
Cihan Caglayan <hocheung20@gmail.com>
2009-08-07 03:06:22 +0000

    Added new version of the undoBuffer, this one is has been tested using the main function in the implementation file.

---
+++ Tracing/TraceEdit/undobuff.cxx
@@ -0,0 +1,46 @@
+#include "undobuffer.h"
+
+/*
+int main(int argc, char *argv[])
+{
+	undoBuffer<std::string> buffer;
+	std::string testString = "ABCDEFGHIJ";
+	for(unsigned int i = 0; i < 10; i++)
+	{
+		buffer.Add(testString.substr(i, 1));
+		buffer.print();
+	}
+	std::cout << std::endl;
+	
+	for(unsigned int i = 0; i < 11; i++)
+	{
+		if(buffer.UndoOrRedo(0))
+		{
+			std::cout << "Current Pos: " << buffer.getCurrPos() << std::endl;
+			std::cout << "Current state: " << buffer.getState() << std::endl;
+			buffer.print();
+			std::cout << std::endl;
+		}
+	}
+		
+	for(unsigned int i = 0 ; i < 12; i++)
+	{
+		if(buffer.UndoOrRedo(1))
+		{
+			std::cout << "Current Pos: " << buffer.getCurrPos() << std::endl;
+			std::cout << "Current State: " << buffer.getState() << std::endl;
+			buffer.print();
+			std::cout << std::endl;
+		}
+	}
+		
+	std::string test3 = "1234567890";
+	for(unsigned int i = 0; i < 10; i++)
+	{
+		buffer.Add(test3.substr(i,1));
+		buffer.print();
+	}
+	std::cout << std::endl;
+	return 0;
+}
+*/
