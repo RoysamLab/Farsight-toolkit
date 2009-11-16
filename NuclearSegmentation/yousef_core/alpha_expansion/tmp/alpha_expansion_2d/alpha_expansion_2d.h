52bb5b983a41517ac9d33d9dfcc4c40d1ec1e475
Zack Galbreath <zack.galbreath@gmail.com>
2009-11-16 21:15:51 +0000

    STYLE: change NuclearSegmentation license from Apache to GPL

--- NuclearSegmentation/yousef_core/alpha_expansion/tmp/alpha_expansion_2d/alpha_expansion_2d.h
+++ NuclearSegmentation/yousef_core/alpha_expansion/tmp/alpha_expansion_2d/alpha_expansion_2d.h
@@ -1,3 +1,20 @@
+/* 
+ * Copyright 2009 Rensselaer Polytechnic Institute
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #ifndef ALPHA_EXPANSION_2D_H
 #define ALPHA_EXPANSION_2D_H
 
