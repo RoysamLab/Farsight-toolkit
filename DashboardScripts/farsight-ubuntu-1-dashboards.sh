393c6a3a1b0ced72cbcab32ea87fd3d22d7f5cd5
Zack Galbreath <zack.galbreath@gmail.com>
2009-12-07 15:47:26 +0000

    ENH: nightly builds on farsight-ubuntu-1

---
+++ DashboardScripts/farsight-ubuntu-1-dashboards.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc41_rel_static_nightly.cmake > /Dashboards/Logs/rel_static_nightly.log 2>&1
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc41_dbg_static_nightly.cmake > /Dashboards/Logs/dbg_static_nightly.log 2>&1
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc41_dbg_static_continuous.cmake > /Dashboards/Logs/dbg_static_continuous.log 2>&1
