fbdd75ded28757b076d33be71659a1c8391ba6f6
Kedar Grama <gbkedar@gmail.com>
2009-12-24 17:47:32 +0000

    BUG: Fixed warnings on dashboard

--- NuclearSegmentation/Nuclear_Association/AssociationAuxFns.cpp
+++ NuclearSegmentation/Nuclear_Association/AssociationAuxFns.cpp
@@ -1,5 +1,30 @@
-#ifndef _EC_FEATURES_CPP_
-#define _EC_FEATURES_CPP_
+/* 
+ * Copyright 2009 Rensselaer Polytechnic Institute
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*=========================================================================
+
+  Program:   Farsight Biological Image Segmentation and Visualization Toolkit
+  Language:  C++
+  Date:      $Date:  $
+  Version:   $Revision: 0.00 $
+
+=========================================================================*/
+#ifndef _ASC_FEAT_AUX_FN_CPP_
+#define _ASC_FEAT_AUX_FN_CPP_
 
 #include <math.h>
 
@@ -9,10 +34,11 @@
 #include "itkNumericTraits.h"
 #include "itkOtsuMultipleThresholdsCalculator.h"
 #include "itkLabelStatisticsImageFilter.h"
+
 #include "ftkCommon/itkLabelGeometryImageFilter.h"
 
-#define MM_PI       3.14159265358979323846
-#define MM_PI_2     1.57079632679489661923
+#define MM_PI		3.14159265358979323846
+#define MM_PI_2		1.57079632679489661923
 
 typedef unsigned short USPixelType;
 typedef itk::Image< USPixelType, 2 > UShortImageType;
@@ -80,6 +106,34 @@ std::vector<float> compute_ec_features( UShortImageType::Pointer input_image,  U
 		for( int j=0; j<number_of_rois; j++ ) quantified_numbers.push_back(quantified_numbers_cell[j]);
 	}
 	return quantified_numbers;
+}
+
+unsigned short returnthresh( UShortImageType::Pointer input_image, int num_bin_levs, int num_in_fg ){
+//Instantiate the different image and filter types that will be used
+	typedef itk::ImageRegionIteratorWithIndex< UShortImageType > IteratorType;
+	typedef itk::ImageRegionConstIterator< UShortImageType > ConstIteratorType;
+	typedef itk::Statistics::ScalarImageToHistogramGenerator< UShortImageType > ScalarImageToHistogramGeneratorType;
+	typedef ScalarImageToHistogramGeneratorType::HistogramType HistogramType;
+	typedef itk::OtsuMultipleThresholdsCalculator< HistogramType > CalculatorType;
+
+	ScalarImageToHistogramGeneratorType::Pointer scalarImageToHistogramGenerator = ScalarImageToHistogramGeneratorType::New();
+	CalculatorType::Pointer calculator = CalculatorType::New();
+	scalarImageToHistogramGenerator->SetNumberOfBins( 128 );
+	calculator->SetNumberOfThresholds( num_bin_levs );
+	scalarImageToHistogramGenerator->SetInput( input_image);
+	scalarImageToHistogramGenerator->Compute();
+	calculator->SetInputHistogram( scalarImageToHistogramGenerator->GetOutput() );
+	calculator->Update();
+	const CalculatorType::OutputType &thresholdVector = calculator->GetOutput(); 
+	CalculatorType::OutputType::const_iterator itNum = thresholdVector.begin();
+
+	USPixelType thresh;
+
+	for(int i=0; i < num_in_fg; ++itNum, ++i)
+		thresh = (static_cast<USPixelType>(*itNum));
+
+	return thresh;
+
 }
 
 #endif
