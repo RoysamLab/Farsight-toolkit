d91d1da4e58bedb077d2c4169f9c90423fd85309
Zack Galbreath <zack.galbreath@gmail.com>
2009-12-29 14:52:51 +0000

    ENH: configure the mac to perform automatic builds of farsight

---
+++ DashboardScripts/farsight-mac-1-dashboards.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/local/bin/ctest -V -VV -S /projects/Dashboards/farsight-trunk/DashboardScripts/farsight-mac-1_nightly_release_makefiles.cmake > /projects/Dashboards/Logs/nightly_release_makefiles.log 2>&1
+/usr/local/bin/ctest -V -VV -S /projects/Dashboards/farsight-trunk/DashboardScripts/farsight-mac-1_nightly_debug_makefiles.cmake > /projects/Dashboards/Logs/nightly_debug_makefiles.log 2>&1
+/usr/local/bin/ctest -V -VV -S /projects/Dashboards/farsight-trunk/DashboardScripts/farsight-mac-1_continuous_debug_makefiles.cmake > /projects/Dashboards/Logs/continuous_debug_makefiles.log 2>&1
+
