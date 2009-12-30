8ae017e8dac2101697c6e44dc59b7d09c2618e4a
Xiao Liang <hocheung20@gmail.com>
2009-12-30 20:29:25 +0000

    ENH:out put head file for raw data

--- Tracing/MDL/ObjectVolumeProcess.cpp
+++ Tracing/MDL/ObjectVolumeProcess.cpp
@@ -156,6 +156,32 @@ int main(int argc, char *argv[])
 
   //write the output image and free memory
   fwrite(volout, sizeX*sizeY*sizeZ, sizeof(DATATYPEOUT), outfile);
+
+  FILE *mhdfile;
+  if((mhdfile=fopen("ObjectPreProcessing.mhd","w"))==NULL)
+    {
+    cerr << "output file open error!" << endl;
+    return -1;
+    }
+  fprintf (mhdfile,"ObjectType = Image\n");
+  fprintf (mhdfile,"NDims = 3\n");
+  fprintf (mhdfile,"BinaryData = True\n");
+  fprintf (mhdfile,"BinaryDataByteOrderMSB = False\n");
+  fprintf (mhdfile,"CompressedData = False\n");
+  fprintf (mhdfile,"TransformMatrix = 1 0 0 0 1 0 0 0 1\n");
+  fprintf (mhdfile,"Offset = 0 0 0\n");
+  fprintf (mhdfile,"CenterOfRotation = 0 0 0\n");
+  fprintf (mhdfile,"AnatomicalOrientation = RAI\n");
+  fprintf (mhdfile,"ElementSpacing = 1 1 1\n");
+  fprintf (mhdfile,"DimSize = %d %d %d\n",sizeX,sizeY,sizeZ);
+  fprintf (mhdfile,"ElementType = MET_UCHAR\n");
+  fprintf (mhdfile,"ElementDataFile = ObjectPreProcessing.raw\n");
+  fclose(mhdfile);
+
+
+  
+
+
   free(volout); 
   volout=NULL;
 
