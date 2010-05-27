69efae25773f6c208ae8a15f31d4acaedfa73b1f
Zack Galbreath <zack.galbreath@gmail.com>
2010-05-27 21:16:26 +0000

    ENH: first step towards a windows dashboard

---
+++ DashboardScripts/farsight-vista.bat
@@ -0,0 +1,1 @@
+call "C:\Program Files (x86)\CMake 2.8\bin\ctest.exe -S "C:\Dashboards\farsight-trunk\DashboardScripts\farsight-vista_dbg_nightly.cmake" -VV -O C:\Dashboards\farsight_dbg_nightly_log.txt
