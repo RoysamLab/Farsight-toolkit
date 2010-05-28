c2816aa394ac6fd9e1afa4f27b7c6dbf6b012637
Zack Galbreath <zack.galbreath@gmail.com>
2010-05-28 13:52:18 +0000

    BUG: get the farsight-ubuntu-1 dashboards to run again

--- DashboardScripts/farsight-ubuntu-1-dashboards.sh
+++ DashboardScripts/farsight-ubuntu-1-dashboards.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export DISPLAY=:0.0
-/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc41_rel_static_nightly.cmake > /Dashboards/Logs/rel_static_nightly.log 2>&1
-/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc41_dbg_static_nightly.cmake > /Dashboards/Logs/dbg_static_nightly.log 2>&1
-/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc41_dbg_static_continuous.cmake > /Dashboards/Logs/dbg_static_continuous.log 2>&1
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc43_rel_static_nightly.cmake > /Dashboards/Logs/rel_static_nightly.log 2>&1
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc43_dbg_static_nightly.cmake > /Dashboards/Logs/dbg_static_nightly.log 2>&1
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc43_dbg_static_nightly_vtkgit.cmake > /Dashboards/Logs/dbg_static_nightly.log 2>&1
+/usr/local/bin/ctest -V -VV -S /Dashboards/farsight-trunk/DashboardScripts/farsight-ubuntu-1_gcc43_dbg_static_continuous.cmake > /Dashboards/Logs/dbg_static_continuous.log 2>&1
