534b5a075de7c1877cf09ec63e6d35ad2aaf4b9f
Tenicka Turnquest <hocheung20@gmail.com>
2010-06-14 18:19:40 +0000

    Edited Usage file

--- Tracing/MDL_XL/Demo_Bspline.cpp
+++ Tracing/MDL_XL/Demo_Bspline.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 {
 	if(argc < 3)
     {
-		std::cerr << "Usage: " << argv[0] << "inputImageFile" << " inputVtkFile" << "outputVtkFile" <<std::endl;
+		std::cerr << "Usage: " << argv[0] << " inputImageFile  inputVtkFile  outputVtkFile " <<std::endl;
 		return EXIT_FAILURE;
     }
     std::string inputImageFile = argv[1];
