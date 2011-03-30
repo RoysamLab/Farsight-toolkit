15726c12b64cf3e3be14b873c5ffafb12c58278f
Raghav Padmanabhan <raghav.krishna@gmail.com>
2011-03-30 18:04:59 +0000

    Qhull Library fro computing convexity of cells

---
+++ ftkFeatures/Qhull/qhull.h
@@ -0,0 +1,31 @@
+/*<html><pre>  -<a                             href="qh-qhull.htm"
+  >-------------------------------</a><a name="TOP">-</a>
+
+   qhull.h
+
+   Proxy for libqhull.h for backwards compatability
+
+   copyright (c) 1993-2010 The Geometry Center.
+   $Id: //product/qhull/main/rel/src/qhull.h#51 $$Change: 1188 $
+   $DateTime: 2010/01/14 22:35:43 $$Author: bbarber $
+*/
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifndef qhDEFqhull
+#define qhDEFqhull 1
+
+
+
+#include "libqhull.h"
+
+
+#endif /* qhDEFqhull */
+
+#ifdef __cplusplus
+ }
+ #endif 
