bdbd3f15896f2fb8336c52df299975e577fa4f24
Raghav Padmanabhan <raghav.krishna@gmail.com>
2011-03-30 18:15:26 +0000

    ENH: Made changes to reflect the changes in Stat_Model_Segmentation files

--- ftkCommon/ftkgnt.h
+++ ftkCommon/ftkgnt.h
@@ -42,29 +42,37 @@ public:
   typedef boost::graph_traits<RAGraph>::adjacency_iterator AdjVertIt;
   typedef property_map<MTreeType,vertex_name_t>::type nodes_new; 
   typedef graph_traits<MTreeType>::vertex_descriptor node_mt;
-
+  typedef ftk::IntrinsicFeatures FeaturesType;	
 
     //: constructor
-	ftkgnt(InputImageType::Pointer input,OutputImageType::Pointer output);
+	ftkgnt();
  	//: destructor
 	~ftkgnt();
   	
 RAGraph RAG;
 RAGraph BuildRAG(unsigned short id);
+//RAGraph BuildRAG(unsigned short id,InputImageType::Pointer input,OutputImageType::Pointer output);
 int GetNodeIndex(unsigned short id,RAGraph graph1);	
 MTreeType BuildMergeTreeDcon(RAGraph R1, unsigned short id,std::vector< std::set<int> > hypothesis);
 std::string convert2string(unsigned short id);
-void ftkgnt::Initialize(unsigned short id);
-
+void Initialize(unsigned short id);
+void runLabFilter(InputImageType::Pointer input,OutputImageType::Pointer output);
 std::vector< std::set<int> > hypotheses;
-inline std::vector< std::set<int> > getHypothesis() {return hypotheses;};
+//inline std::vector< std::set<int> > getHypothesis() {return hypotheses;};
 FeatureCalcType::Pointer labelFilter;
+std::vector<FeaturesType> allFeat;
+std::vector< unsigned short > labelIndex;
+void setFeats(std::vector<FeaturesType> allFeat,std::vector< unsigned short > labelIndex);
+
+void setmaxVol(double vol);
+void setmaxDepth(int depth);
 
 
 private:
 
+	double MAX_VOL;
+	int MAX_DEPTH;
 	
-
 };
 
 
