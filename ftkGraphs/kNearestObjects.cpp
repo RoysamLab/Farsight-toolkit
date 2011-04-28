2d5f84744e7bfe0e72c6731a5ec50adbd5ae4867
Vinay Somasundar <vinay.somasundar@gmail.com>
2011-04-28 17:45:19 +0000

    ENH: Addition of the logic to generate Neighbors within radius and making this and K nearest neighbor query open ended

--- ftkGraphs/kNearestObjects.cpp
+++ ftkGraphs/kNearestObjects.cpp
@@ -4,13 +4,11 @@
 kNearestObjects::kNearestObjects(std::map< unsigned int, std::vector<float> > centroidMap)
 {
 	allIds = 0;
+	featureTable = NULL;
 	this->centerMap = centroidMap;
 	sample = SampleType::New();
 	treeGenerator = TreeGeneratorType::New();
-}
 
-std::vector<std::vector<unsigned int>> kNearestObjects::k_nearest_neighbors_All(unsigned int k)
-{
 	sample->SetMeasurementVectorSize( 3 );
 
 	MeasurementVectorType mv;
@@ -30,19 +28,133 @@ std::vector<std::vector<unsigned int>> kNearestObjects::k_nearest_neighbors_All(
 	treeGenerator->Update();
 
 	tree = treeGenerator->GetOutput();
-	
-	std::vector<std::vector<unsigned int>> kNearestIDs;
+}
+
+
+//***********************************************************************************************
+// K Nearest Neighbors
+//***********************************************************************************************
+
+std::vector< std::vector< std::pair<unsigned int, double> > > kNearestObjects::k_nearest_neighbors_All(unsigned int k, unsigned short Class_dest, unsigned short Class_src)
+{	
+	std::vector<std::vector< std::pair<unsigned int, double> >> kNearestIDs;
 	for(IdIt = idToCentroidMap.begin(); IdIt != idToCentroidMap.end(); ++IdIt )
 	{
-		std::vector<unsigned int> kNearestIds;
-		kNearestIds = k_nearest_neighbors_ID((*IdIt).first, k);
-		kNearestIDs.push_back(kNearestIds);
+		std::vector< std::pair<unsigned int, double> > kNearestIds;
+		unsigned int ID = (*IdIt).first;
+		if(Class_src == 0)
+			kNearestIds = k_nearest_neighbors_ID(ID, k, Class_dest);
+		else
+		{
+			for(int row = 0; row<(int)featureTable->GetNumberOfRows(); ++row)
+			{
+				if( (ID == featureTable->GetValueByName(row, "ID").ToUnsignedInt())
+					&& (featureTable->GetValueByName(row, "prediction_default1").ToUnsignedShort() == Class_src) )
+				{
+					kNearestIds = k_nearest_neighbors_ID(ID, k, Class_dest);
+					kNearestIDs.push_back(kNearestIds);
+					break;
+				}
+			}
+		}		
 	}
 
 	return kNearestIDs;
 }
 
-std::vector<unsigned int> kNearestObjects::k_nearest_neighbors_ID(unsigned int id, unsigned int k)
+std::vector< std::pair<unsigned int, double> > kNearestObjects::k_nearest_neighbors_ID(unsigned int id, unsigned int k, unsigned short Class_dest)
+{
+	std::vector< std::pair<unsigned int, double> > kNearestIds;
+	
+	if(Class_dest == 0)
+	{
+		DistanceMetricType::Pointer distanceMetric = DistanceMetricType::New();
+		DistanceMetricType::OriginType origin( 3 );
+		for (unsigned int i = 0 ; i < (unsigned int)sample->GetMeasurementVectorSize() ; ++i)
+		{
+			origin[i] = idToCentroidMap[id][i];
+		}
+		distanceMetric->SetOrigin( origin );
+
+		TreeType::InstanceIdentifierVectorType kNeighbors;
+		tree->Search( idToCentroidMap[id], k+1, kNeighbors ); 
+		kNearestIds.push_back( std::make_pair(id,0) );
+		for ( unsigned int i = 0 ; i < kNeighbors.size() ; ++i )
+		{
+		    std::map<int, MeasurementVectorType>::iterator iter;
+			MeasurementVectorType mvt = tree->GetMeasurementVector(kNeighbors[i]);
+			for(iter = idToCentroidMap.begin(); iter != idToCentroidMap.end(); ++iter)
+			{
+				if(((*iter).second == mvt) && ((*iter).first!=id))
+				{
+					double dist = distanceMetric->Evaluate( tree->GetMeasurementVector( kNeighbors[i] ));
+					std::pair<unsigned int, double> Neighbor = std::make_pair((*iter).first, dist);
+					kNearestIds.push_back(Neighbor);
+					break;				
+				}
+			}
+		}
+	}
+
+	else
+	{
+		kNearestIds = neighborsWithinRadius_ID(id, 1e5, Class_dest);
+		unsigned int vector_size = (unsigned int)kNearestIds.size();
+		double max_dist;
+		unsigned int max_pos;
+		while((unsigned int)kNearestIds.size() != k+1)
+		{
+			max_dist = 0;
+			for(unsigned int x=1; x<k+1; ++x)
+			{
+				if(kNearestIds.at(x).second > max_dist)
+				{
+					max_dist = kNearestIds.at(x).second;
+					max_pos = x;
+				}
+			}
+			if(kNearestIds.at(k+1).second < max_dist )
+				kNearestIds.erase(kNearestIds.begin()+max_pos);
+			else
+				kNearestIds.erase(kNearestIds.begin()+k+1);
+		}
+	}
+
+	return kNearestIds;
+}
+
+//***********************************************************************************************
+// Neighbors with certain radius
+//***********************************************************************************************
+
+std::vector< std::vector< std::pair<unsigned int, double> > > kNearestObjects::neighborsWithinRadius_All(double radius, unsigned short Class_dest, unsigned short Class_src)
+{	
+	std::vector<std::vector< std::pair<unsigned int, double> >> inRadiusIDs;
+	for(IdIt = idToCentroidMap.begin(); IdIt != idToCentroidMap.end(); ++IdIt )
+	{
+		std::vector< std::pair<unsigned int, double> > inRadiusIds;
+		unsigned int ID = (*IdIt).first;
+		if(Class_src == 0)
+			inRadiusIds = neighborsWithinRadius_ID(ID, radius, Class_dest);
+		else
+		{
+			for(int row = 0; row<(int)featureTable->GetNumberOfRows(); ++row)
+			{
+				if( (ID == featureTable->GetValueByName(row, "ID").ToUnsignedInt())
+					&& (featureTable->GetValueByName(row, "prediction_default1").ToUnsignedShort() == Class_src) )
+				{
+					inRadiusIds = neighborsWithinRadius_ID(ID, radius, Class_dest);
+					inRadiusIDs.push_back(inRadiusIds);
+					break;
+				}
+			}
+		}		
+	}
+
+	return inRadiusIDs;
+}
+
+std::vector< std::pair<unsigned int, double> > kNearestObjects::neighborsWithinRadius_ID(unsigned int id, double radius, unsigned short Class_dest)
 {
 	DistanceMetricType::Pointer distanceMetric = DistanceMetricType::New();
 	DistanceMetricType::OriginType origin( 3 );
@@ -52,80 +164,150 @@ std::vector<unsigned int> kNearestObjects::k_nearest_neighbors_ID(unsigned int i
     }
 	distanceMetric->SetOrigin( origin );
 
-	TreeType::InstanceIdentifierVectorType neighbors;
-	tree->Search( idToCentroidMap[id], k+1, neighbors ); 
-	std::vector<unsigned int> kNearestIds;
-	kNearestIds.push_back(id);
-	for ( unsigned int i = 0 ; i < k+1 ; ++i )
+	TreeType::InstanceIdentifierVectorType radNeighbors;
+	tree->Search( idToCentroidMap[id], radius, radNeighbors ); 
+	std::vector< std::pair<unsigned int, double> > inRadiusIds;
+	inRadiusIds.push_back( std::make_pair(id,0) );
+	for ( unsigned int i = 0 ; i < radNeighbors.size() ; ++i )
 	{
 	    std::map<int, MeasurementVectorType>::iterator iter;
-		MeasurementVectorType mvt = tree->GetMeasurementVector(neighbors[i]);
+		MeasurementVectorType mvt = tree->GetMeasurementVector(radNeighbors[i]);
 		for(iter = idToCentroidMap.begin(); iter != idToCentroidMap.end(); ++iter)
 		{
-			if((*iter).second == mvt)
+			if(((*iter).second == mvt) && ((*iter).first!=id))
 			{
-				if((*iter).first!=id)
+				if(Class_dest == 0)
 				{
-					kNearestIds.push_back((*iter).first);
+					double dist = distanceMetric->Evaluate( tree->GetMeasurementVector( radNeighbors[i] ));
+					std::pair<unsigned int, double> Neighbor = std::make_pair((*iter).first, dist);
+					inRadiusIds.push_back(Neighbor);
+					break;
+				}
+				else
+				{
+					for(int row = 0; row<(int)featureTable->GetNumberOfRows(); ++row)
+					{
+						if( ((*iter).first == featureTable->GetValueByName(row, "ID").ToUnsignedInt())
+							&& (featureTable->GetValueByName(row, "prediction_default1").ToUnsignedShort() == Class_dest) )
+						{
+							double dist = distanceMetric->Evaluate( tree->GetMeasurementVector( radNeighbors[i] ));
+							std::pair<unsigned int, double> Neighbor = std::make_pair((*iter).first, dist);
+							inRadiusIds.push_back(Neighbor);
+							break;
+						}
+					}
 					break;
 				}
 			}
 		}
 	}
 
-	return kNearestIds;
+	return inRadiusIds;
 }
 
-kNearestObjects::kNeighborGraph kNearestObjects::kNearestGraph(std::vector<unsigned int> kNearestIds)
+
+//***********************************************************************************************
+// Build Graph from Vectors
+//***********************************************************************************************
+vtkSmartPointer<vtkTable> kNearestObjects::vectorsToGraphTable(std::vector< std::vector< std::pair<unsigned int, double> > > NeighborIDs)
 {
-	if(allIds == 0)
-	{
-		KNG.clear();
-		nodeName  = get(vertex_name, KNG);
-	}
+	graphtable = vtkSmartPointer<vtkTable>::New();
+	graphtable->Initialize();
+	vtkSmartPointer<vtkStringArray> column1 = vtkSmartPointer<vtkStringArray>::New();
+    column1->SetName( "Source" );
+    graphtable->AddColumn(column1);
+    vtkSmartPointer<vtkStringArray> column2 = vtkSmartPointer<vtkStringArray>::New();
+    column2->SetName( "Target" );
+	graphtable->AddColumn(column2);
 
-	int src = GetNodeIndex(kNearestIds.at(0));
-	if(src == -1)
-	{
-		node S;
-		S = add_vertex(KNG);
-		nodeName[S] = convert2string(kNearestIds.at(0));
-		src = num_vertices(KNG)-1;
-	}
-
-	for(unsigned int i=1 ; i<kNearestIds.size() ; i++)
-	{
-		int trg = GetNodeIndex(kNearestIds[i]);
-		if(trg ==-1)
-		{
-			node T;
-			T = add_vertex(KNG);
-			nodeName[T] = convert2string(kNearestIds[i]);
-			trg = num_vertices(KNG)-1;
-		}
-		bool bRet;
-		Edge e;
-		tie(e,bRet) = edge(src,trg,KNG);
-		if(!bRet)
-		{
-			add_edge(src,trg,KNG);
-		}
-	}
-
-	return KNG;
-}
-
-kNearestObjects::kNeighborGraph kNearestObjects::kNearestGraph(std::vector<std::vector<unsigned int>> kNearestIDs)
-{
-	nodeName  = get(vertex_name, KNG);	
 	allIds = 1;
-	for(unsigned int j=0 ; j<kNearestIDs.size() ; j++)
+	for(unsigned int j=0 ; j<NeighborIDs.size() ; j++)
 	{
-		this->kNearestGraph(kNearestIDs[j]);
+		this->vectorsToGraphTable(NeighborIDs[j]);
 	}
 	allIds = 0;
 
-	return KNG;
+	return graphtable;
+}
+
+vtkSmartPointer<vtkTable> kNearestObjects::vectorsToGraphTable(std::vector< std::pair<unsigned int, double> > NeighborIds)
+{
+	if(allIds == 0)
+	{
+		graphtable = vtkSmartPointer<vtkTable>::New();
+		graphtable->Initialize();
+		vtkSmartPointer<vtkStringArray> column1 = vtkSmartPointer<vtkStringArray>::New();
+		column1->SetName( "Source" );
+		graphtable->AddColumn(column1);
+		vtkSmartPointer<vtkStringArray> column2 = vtkSmartPointer<vtkStringArray>::New();
+		column2->SetName( "Target" );
+		graphtable->AddColumn(column2);
+	}
+	
+	for(unsigned int i=1 ; i<(int)NeighborIds.size() ; i++)
+	{
+		vtkSmartPointer<vtkVariantArray> model_data1 = vtkSmartPointer<vtkVariantArray>::New();
+     	model_data1->InsertNextValue(static_cast<vtkVariant>(NeighborIds.at(0).first));
+		model_data1->InsertNextValue(static_cast<vtkVariant>(NeighborIds.at(i).first));
+		graphtable->InsertNextRow(model_data1);	
+	}
+	return graphtable;
+}
+
+//***********************************************************************************************
+// Build Graph from Vectors
+//***********************************************************************************************
+
+kNearestObjects::NeighborGraph kNearestObjects::getNeighborGraph(std::vector< std::vector< std::pair<unsigned int, double> > > NeighborIDs)
+{
+	nodeName  = get(vertex_name, NG);	
+	allIds = 1;
+	for(unsigned int j=0 ; j<NeighborIDs.size() ; j++)
+	{
+		this->getNeighborGraph(NeighborIDs[j]);
+	}
+	allIds = 0;
+
+	return NG;
+}
+
+kNearestObjects::NeighborGraph kNearestObjects::getNeighborGraph(std::vector< std::pair<unsigned int, double> > NeighborIds)
+{
+	if(allIds == 0)
+	{
+		NG.clear();
+		nodeName  = get(vertex_name, NG);
+	}
+
+	int src = GetNodeIndex(NeighborIds.at(0).first);
+	if(src == -1)
+	{
+		node S;
+		S = add_vertex(NG);
+		nodeName[S] = convert2string(NeighborIds.at(0).first);
+		src = num_vertices(NG)-1;
+	}
+
+	for(unsigned int i=1 ; i<(int)NeighborIds.size() ; i++)
+	{
+		int trg = GetNodeIndex(NeighborIds[i].first);
+		if(trg ==-1)
+		{
+			node T;
+			T = add_vertex(NG);
+			nodeName[T] = convert2string(NeighborIds[i].first);
+			trg = num_vertices(NG)-1;
+		}
+		bool bRet;
+		Edge e;
+		tie(e,bRet) = edge(src,trg,NG);
+		if(!bRet)
+		{
+			add_edge(src,trg,NG);
+		}
+	}
+
+	return NG;
 }
 
 std::string kNearestObjects::convert2string(unsigned int id)
@@ -141,7 +323,7 @@ int kNearestObjects::GetNodeIndex(unsigned int id)
 	std::string id1 = convert2string(id);
 	int index;
 	int flag = -1;
-	for ( unsigned int i = 0 ; i < num_vertices(KNG) ; ++i)
+	for ( unsigned int i = 0 ; i < num_vertices(NG) ; ++i)
 	{
 		if(nodeName[i]==id1)
 		{
@@ -161,9 +343,13 @@ int kNearestObjects::GetNodeIndex(unsigned int id)
 	}
 }
 
-vtkSmartPointer<vtkTable> kNearestObjects::kNeighborTable(kNeighborGraph g)
+//***********************************************************************************************
+// Build Table from graph
+//***********************************************************************************************
+
+vtkSmartPointer<vtkTable> kNearestObjects::graphToTable(NeighborGraph g)
 {
-	vtkSmartPointer<vtkTable> graphtable = vtkSmartPointer<vtkTable>::New();
+	graphtable = vtkSmartPointer<vtkTable>::New();
 	graphtable->Initialize();
 	vtkSmartPointer<vtkStringArray> column1 = vtkSmartPointer<vtkStringArray>::New();
     column1->SetName( "Source" );
@@ -176,24 +362,23 @@ vtkSmartPointer<vtkTable> kNearestObjects::kNeighborTable(kNeighborGraph g)
     {
 	    for (tie(ai,ai_end) = adjacent_vertices(*vi, g) ; ai != ai_end ; ++ai)
 	    {
-            check = 1;
-	        for(int i=0; i<(int)graphtable->GetNumberOfRows(); ++i)
-	        {
-		        //std::cout<<i<<std::endl;
-		        if(((static_cast<vtkVariant>(nodeName[*vi])==graphtable->GetValue(i,0))&&(static_cast<vtkVariant>(nodeName[*ai])==graphtable->GetValue(i,1))) || 
-		 	       ((static_cast<vtkVariant>(nodeName[*ai])==graphtable->GetValue(i,0))&&(static_cast<vtkVariant>(nodeName[*vi])==graphtable->GetValue(i,1))))
-		        {
-		 	        check = 0;
-		 	        break;
-		        }
- 	        }
-	        if(check == 1)
-	        {
+         //   check = 1;
+	        //for(int i=0; i<(int)graphtable->GetNumberOfRows(); ++i)
+	        //{
+		       // if(((static_cast<vtkVariant>(nodeName[*vi])==graphtable->GetValue(i,0))&&(static_cast<vtkVariant>(nodeName[*ai])==graphtable->GetValue(i,1))) || 
+		 	     //  ((static_cast<vtkVariant>(nodeName[*ai])==graphtable->GetValue(i,0))&&(static_cast<vtkVariant>(nodeName[*vi])==graphtable->GetValue(i,1))))
+		       // {
+		 	     //   check = 0;
+		 	     //   break;
+		       // }
+ 	       // }
+	        //if(check == 1)
+	        //{
 	            vtkSmartPointer<vtkVariantArray> model_data1 = vtkSmartPointer<vtkVariantArray>::New();
      	        model_data1->InsertNextValue(static_cast<vtkVariant>(nodeName[*vi]));
 		        model_data1->InsertNextValue(static_cast<vtkVariant>(nodeName[*ai]));
 		        graphtable->InsertNextRow(model_data1);		  
-	        }	   
+	        //}	   
         }
     }
     return graphtable;
