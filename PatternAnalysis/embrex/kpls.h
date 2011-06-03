911e8c762a0aecfd57976b0880fab4571c34f1fc
Vinay Somasundar <vinay.somasundar@gmail.com>
2011-06-03 18:56:30 +0000

    BUGFIX: #ifndef was changed to KPLS_H

--- PatternAnalysis/embrex/kpls.h
+++ PatternAnalysis/embrex/kpls.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 =========================================================================*/
 
-#ifndef NUCLEUS_EDITOR_H
-#define NUCLEUS_EDITOR_H
+#ifndef KPLS_H
+#define KPLS_H
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
