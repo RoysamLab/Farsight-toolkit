1c9d9bb27148f818970c0069d8989b46faa9842d
Vinay Somasundar <vinay.somasundar@gmail.com>
2011-07-25 20:58:27 +0000

    ENH: Now the class returns the zernike moments as a vector

--- Zernike/zernike.h
+++ Zernike/zernike.h
@@ -1,8 +1,14 @@
-#include "itkImageRegionIteratorWithIndex.h"
-#include "itkImageRegionConstIterator.h"
-#include "itkImageRegionIterator.h"
+#ifndef __Zernike_h
+#define __Zernike_h
+
+#include <iostream>
+#include <math.h>
+
 #include "itkImage.h"
-#include <itkIndex.h>
+#include "itkIndex.h"
+#include "itkImageRegionIteratorWithIndex.h"
+//#include "itkImageRegionConstIterator.h"
+//#include "itkImageRegionIterator.h"
 #include "itkImageFileReader.h"
 
 
@@ -16,11 +22,11 @@ class zernike
 {
 	public:
 
-		#define Dimension 2
+		#define Dimen 2
 		#define VIndexDimension 2
 
 		typedef unsigned char PixelType;
-		typedef itk::Image<PixelType , Dimension > ImageType;
+		typedef itk::Image<PixelType , Dimen > ImageType;
 		typedef itk::ImageRegionIteratorWithIndex< ImageType > IteratorType;
 		typedef itk::Index< VIndexDimension > IndexType;
 		typedef ImageType::SizeType InputSizeType;
@@ -34,26 +40,26 @@ class zernike
 		zernike(ImageType::Pointer inputImage,int orderofmoments);
 		zernike::zernike();
 		zernike::~zernike();		
-		void GetZernike();
+		std::vector< std::vector<double> > GetZernike();
 		
     private: 	    	
 	   
 		int ReadInputImage(std::string imageFileName);
-		/* this method is used to set the input image parameters using  Image Region Iterators as we need image dimensions (M and N)  for the calculations of Zernike moments and need to access the image pixel data */
+		// this method is used to set the input image parameters using  Image Region Iterators as we need image dimensions (M and N)  for the calculations of Zernike moments and need to access the image pixel data
 		void SetInputImage();
 		int M , N ;
 		unsigned char ** f;
 	
-		/* this methog gives the values of X coordinates in the  image*/
+		// this methog gives the values of X coordinates in the  image
 		double* getX();
-		/* this methog gives the values of Y coordinates in the mapped image*/
+		// this methog gives the values of Y coordinates in the mapped image
 		double* getY();
-		/*this method returns the (0,0) order geometrical moment which is equal to sum(sum(f(x,y));*/
+		//this method returns the (0,0) order geometrical moment which is equal to sum(sum(f(x,y));
 		double G00();
-		/*this method returns the (1,0) order geometrical moment which is needed to calculate centre of floroscence for the image ( i.e. X component of  centre of mass in image is is G10()/G00()); */
+		//this method returns the (1,0) order geometrical moment which is needed to calculate centre of floroscence for the image ( i.e. X component of  centre of mass in image is is G10()/G00()); 
 
 		double COF_X();
-		/*this method returns the (0,1) order geometrical moment which is needed to calculate the  centre of fluoroscence for the image ( i.e. Y component of  centre of mass in image is is G01()/G00()); */
+		//this method returns the (0,1) order geometrical moment which is needed to calculate the  centre of fluoroscence for the image ( i.e. Y component of  centre of mass in image is is G01()/G00()); 
 		double COF_Y();
 		double beta();
 		//Rho -> go to the normalized POLAR  coordinates (RHO AND THETA)
@@ -66,3 +72,6 @@ class zernike
 		double* CalculateZernike(int p , int q);
 
 };
+
+
+#endif
