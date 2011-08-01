f82d6a017d495b6008137c7401f5b13f5d7b3a32
Raghav Padmanabhan <raghav.krishna@gmail.com>
2011-08-01 21:22:14 +0000

    ENH: Made changes for table size check

--- ftkGUI/TrainingDialog.h
+++ ftkGUI/TrainingDialog.h
@@ -50,8 +50,10 @@ class TrainingDialog : public QDialog
     Q_OBJECT;
 
 public:
-	TrainingDialog(vtkSmartPointer<vtkTable> table, const char * trainColumn,std::string mode, QWidget *parent = 0);
+	TrainingDialog::TrainingDialog(vtkSmartPointer<vtkTable> table, const char * trainColumn,std::string mode,int firstTableSize, QWidget *parent=0);
+	//TrainingDialog(vtkSmartPointer<vtkTable> table, const char * trainColumn,std::string mode, QWidget *parent = 0);
 	void loadModelFromFile(std::string file_name);
+	int firstTableRows;
 
 public slots:
 	  void accept();
