70f7202f32a4433e1cbb2b86edf04b47e88c3e34
Kedar Grama <gbkedar@gmail.com>
2011-08-28 07:30:41 +0000

    Modifications to dashboard script

--- DashboardScripts/farsight-windows7_dbg_static_nightly.cmake
+++ DashboardScripts/farsight-windows7_dbg_static_nightly.cmake
@@ -14,11 +14,14 @@ SET(CTEST_CMAKE_COMMAND
   "\"c:/Program Files (x86)/CMake 2.8/bin/cmake.exe\""
   )
 
+SET( CMPLR_PATH "C:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE/devenv.com" )
+SET( FPROJ_PATH "C:/dashboard/bin/farsight-static/Farsight.sln" )
+
 SET(CTEST_INITIAL_CACHE "
 SITE:STRING=farsight-win_7_64
 BUILDNAME:STRING=vs9-64-dbg-static
 CMAKE_GENERATOR:INTERNAL=Visual Studio 9 2008 Win64
-MAKECOMMAND:STRING=C:/PROGRA~2/MICROS~10/Common7/IDE/devenv.com Farsight.sln /build Debug /project ALL_BUILD
+MAKECOMMAND:STRING=${CMPLR_PATH} ${FPROJ_PATH} /build Debug /project ALL_BUILD
 BUILD_SHARED_LIBS:BOOL=OFF
 ITK_DIR:PATH=C:/dashboard/bin/itkv4.0a09
 VTK_DIR:PATH=C:/dashboard/bin/vtk-5.6.1
@@ -28,4 +31,3 @@ QT_QMAKE_EXECUTABLE:FILEPATH=C:/Qt/4.7.2/bin/qmake.exe
 ")
 
 SET(CTEST_CVS_COMMAND "C:/Program Files/TortoiseSVN/bin/TortoiseProc.exe")
-
