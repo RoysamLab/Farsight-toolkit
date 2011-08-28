0343d609b4853a9a3f60c73b7b4783cd99aac80e
Kedar Grama <gbkedar@gmail.com>
2011-08-28 09:55:44 +0000

    Added scripts for nightly builds of VXL,ITK and VTK and added an additional build for nightly experimental farsight

--- DashboardScripts/farsight-windows7.bat
+++ DashboardScripts/farsight-windows7.bat
@@ -1,1 +1,7 @@
 call "C:\Program Files (x86)\CMake 2.8\bin\ctest.exe" -S C:/dashboard/src/farsight/DashboardScripts/farsight-windows7_dbg_static_nightly.cmake -VV -O C:/dashboard/farsight_dbg_static_nightly_log.txt
+cd C:/dashboard/src/vxl
+svn up
+call "C:\Program Files (x86)\CMake 2.8\bin\ctest.exe" -S C:/dashboard/src/farsight/DashboardScripts/farsight-windows7_vxl_dbg_nightly.cmake -VV -O C:/dashboard/vxl_dbg_nightly_log.txt
+call "C:\Program Files (x86)\CMake 2.8\bin\ctest.exe" -S C:/dashboard/src/farsight/DashboardScripts/farsight-windows7_itk_dbg_nightly.cmake -VV -O C:/dashboard/itk_dbg_nightly_log.txt
+call "C:\Program Files (x86)\CMake 2.8\bin\ctest.exe" -S C:/dashboard/src/farsight/DashboardScripts/farsight-windows7_vtk_dbg_nightly.cmake -VV -O C:/dashboard/vtk_dbg_nightly_log.txt
+call "C:\Program Files (x86)\CMake 2.8\bin\ctest.exe" -S C:/dashboard/src/farsight/DashboardScripts/farsight-windows7_dbg_nightly.cmake -VV -O C:/dashboard/farsight_dbg_nightly_log.txt
---
+++ DashboardScripts/farsight-windows7_dbg_nightly.cmake
@@ -0,0 +1,33 @@
+SET(CTEST_SOURCE_NAME "src/farsight")
+SET(CTEST_BINARY_NAME "bin/farsight-nightly")
+SET(CTEST_DASHBOARD_ROOT "C:/dashboard")
+SET(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${CTEST_SOURCE_NAME}")
+SET(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${CTEST_BINARY_NAME}")
+
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+SET(CTEST_COMMAND
+  "\"c:/Program Files (x86)/CMake 2.8/bin/ctest.exe\" -V -VV -D Nightly -A \"${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}\""
+  )
+
+SET(CTEST_CMAKE_COMMAND
+  "\"c:/Program Files (x86)/CMake 2.8/bin/cmake.exe\""
+  )
+
+SET( CMPLR_PATH "C:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE/devenv.com" )
+SET( FPROJ_PATH "C:/dashboard/bin/farsight-nightly/Farsight.sln" )
+
+SET(CTEST_INITIAL_CACHE "
+SITE:STRING=farsight-win_7_64
+BUILDNAME:STRING=vs9-64-dbg-nightly
+CMAKE_GENERATOR:INTERNAL=Visual Studio 9 2008 Win64
+MAKECOMMAND:STRING=${CMPLR_PATH} ${FPROJ_PATH} /build Debug /project ALL_BUILD
+BUILD_SHARED_LIBS:BOOL=OFF
+ITK_DIR:PATH=C:/dashboard/bin/itk-nightly
+VTK_DIR:PATH=C:/dashboard/bin/vtk-nightly
+VXL_DIR:PATH=C:/dashboard/bin/vxl-nightly
+Boost_INCLUDE_DIR:PATH=C:/dashboard/src/boost
+QT_QMAKE_EXECUTABLE:FILEPATH=C:/Qt/4.7.2/bin/qmake.exe
+")
+
+SET(CTEST_CVS_COMMAND "C:/Program Files/TortoiseSVN/bin/TortoiseProc.exe")
