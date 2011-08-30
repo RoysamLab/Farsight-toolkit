f1df8947ba4fede50965ce75b589efbe881f3481
Kedar Grama <gbkedar@gmail.com>
2011-08-30 02:55:21 +0000

    git find windows

--- DashboardScripts/farsight-windows7_itk_dbg_nightly.cmake
+++ DashboardScripts/farsight-windows7_itk_dbg_nightly.cmake
@@ -14,6 +14,8 @@ SET(CTEST_CMAKE_COMMAND
   "\"c:/Program Files (x86)/CMake 2.8/bin/cmake.exe\""
   )
 
+find_program(CTEST_GIT_COMMAND NAMES git)
+
 SET( CMPLR_PATH "C:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE/devenv.com" )
 SET( VPROJ_PATH "C:/dashboard/bin/itk-nightly/ITK.sln" )
 SET( PRLL_STR_CXX "/DWIN32 /D_WINDOWS /W3 /Zm1000 /EHsc /GR /MP16 /bigobj" )
@@ -36,4 +38,4 @@ ITK_USE_64BITS_IDS:BOOL=ON
 ")
 
 #SET(CTEST_GIT_COMMAND "C:/Program Files (x86)/Git/cmd/git.cmd")
-find_program(CTEST_GIT_COMMAND NAMES git)
+
--- DashboardScripts/farsight-windows7_vtk_dbg_nightly.cmake
+++ DashboardScripts/farsight-windows7_vtk_dbg_nightly.cmake
@@ -14,6 +14,8 @@ SET(CTEST_CMAKE_COMMAND
   "\"c:/Program Files (x86)/CMake 2.8/bin/cmake.exe\""
   )
 
+find_program(CTEST_GIT_COMMAND NAMES git)
+
 SET( CMPLR_PATH "C:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE/devenv.com" )
 SET( VPROJ_PATH "C:/dashboard/bin/vtk-nightly/VTK.sln" )
 SET( PRLL_STR_CXX "/DWIN32 /D_WINDOWS /W3 /Zm1000 /EHsc /GR /MP16" )
@@ -39,4 +41,4 @@ VTK_USE_BOOST:BOOL=ON
 ")
 
 #SET(CTEST_GIT_COMMAND "C:/Program Files (x86)/Git/cmd/git.cmd")
-find_program(CTEST_GIT_COMMAND NAMES git)
+
