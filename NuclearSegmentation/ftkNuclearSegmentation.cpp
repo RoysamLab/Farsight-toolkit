0262409d012c6cafe9bf6d2e5c8e8cd1d5ea400a
Amine Merouane <aminemer@gmail.com>
2011-11-07 15:41:54 +0000

    chen is testing no change to anything

--- NuclearSegmentation/ftkNuclearSegmentation.cpp
+++ NuclearSegmentation/ftkNuclearSegmentation.cpp
@@ -42,6 +42,7 @@ NuclearSegmentation::NuclearSegmentation()
 	this->ResetAll();
 
 	paramNames.push_back("high_sensitivity");
+
 	paramNames.push_back("LoG_size");
 	paramNames.push_back("min_scale");
 	paramNames.push_back("max_scale");
