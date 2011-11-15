05e2d974d1d60a4b3327dbba8f478fd1e57b533c
Ho Cheung <hocheung20@gmail.com>
2011-11-15 21:14:00 +0000

    Added code to read from SWC file

--- iMontage/main.cpp
+++ iMontage/main.cpp
@@ -5,18 +5,14 @@ int main(int argc, char* argv[])
 {
 	TraceObject* tobj = new TraceObject();
 	
-	//Just for IDE convenience. Remove when done.
-	argc = 2;
-	argv[1] = "montage_8bitkt06041_w311GFPdsu_ANT.swc";
-	
-
 	if (argc < 2)
 	{
 		std::cout << "Must run program with correct number of arguments" << std::endl;
 		return -1;
 	}
-		char* xml_file = argv[1];
-		std::cout << "Reading XML file: " << xml_file << std::endl;
+	
+	char* xml_file = argv[1];
+	std::cout << "Reading XML file: " << xml_file << std::endl;
 	
 	tobj->ReadFromSWCFile(xml_file);
 
