dab41b1a72891a64f8a7072e9b3d535cb60bc3bc
Ho Cheung <hocheung20@gmail.com>
2011-11-21 17:10:38 +0000

    Printing out all the cell bounds

--- iMontage/main.cpp
+++ iMontage/main.cpp
@@ -1,10 +1,12 @@
-#include <TraceObject.h>
+#include "TraceObject.h"
+#include "CellTraceModel.h"
+#include "CellTrace.h"
 #include <string>
 
 int main(int argc, char* argv[])
 {
 	TraceObject* tobj = new TraceObject();
-	
+
 	if (argc < 2)
 	{
 		std::cout << "Must run program with correct number of arguments" << std::endl;
@@ -15,5 +17,17 @@ int main(int argc, char* argv[])
 	std::cout << "Reading XML file: " << xml_file << std::endl;
 	
 	tobj->ReadFromSWCFile(xml_file);
+	CellTraceModel* cells = new CellTraceModel(tobj->CalculateCellFeatures());
 
+	for (int k = 0; k < cells->getCellCount(); k++)
+	{
+		CellTrace* cell = cells->GetCellAt(k);
+		
+		double bounds[6];
+		cell->getCellBounds(bounds);
+
+		//std::cout << "Min X: " << bounds[0] << " Max X: " << bounds[1] << " Min Y: " << bounds[2] << " Max Y: " << bounds[3] << " Min Z: " << bounds[4] << " Max Z: " << bounds[5] << std::endl;
+	}
+
+	return 0;
 }
