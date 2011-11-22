a51c488777239445d7cb13c8f1ff533dcf36befc
Ho Cheung <hocheung20@gmail.com>
2011-11-22 21:50:22 +0000

    iMontage now is able to detect the minimums and maximum of all the bounds in the image

--- iMontage/main.cpp
+++ iMontage/main.cpp
@@ -2,6 +2,7 @@
 #include "CellTraceModel.h"
 #include "CellTrace.h"
 #include <string>
+#include <limits>
 
 int main(int argc, char* argv[])
 {
@@ -19,6 +20,14 @@ int main(int argc, char* argv[])
 	tobj->ReadFromSWCFile(xml_file);
 	CellTraceModel* cells = new CellTraceModel(tobj->CalculateCellFeatures());
 
+	double maxX = 0;
+	double minX = std::numeric_limits<double>::max();
+	double maxY = 0;
+	double minY = std::numeric_limits<double>::max();
+	double maxZ = 0;
+	double minZ = std::numeric_limits<double>::max();
+
+
 	for (int k = 0; k < cells->getCellCount(); k++)
 	{
 		CellTrace* cell = cells->GetCellAt(k);
@@ -27,7 +36,24 @@ int main(int argc, char* argv[])
 		cell->getCellBounds(bounds);
 
 		std::cout << "Min X: " << bounds[0] << " Max X: " << bounds[1] << " Min Y: " << bounds[2] << " Max Y: " << bounds[3] << " Min Z: " << bounds[4] << " Max Z: " << bounds[5] << std::endl;
+	
+		if (bounds[1] > maxX)
+			maxX = bounds[1];
+		if (minX > bounds[0])
+			minX = bounds[0];
+
+		if (bounds[3] > maxY)
+			maxY = bounds[3];
+		if (minY > bounds[2])
+			minY = bounds[2];
+
+		if (bounds[5] > maxZ)
+			maxZ = bounds[5];
+		if (minZ > bounds[4])
+			minZ = bounds[4];
 	}
+
+	std::cout << "Image bounds: (" << minX << ", " << minY << ", " << minZ << ") to (" << maxX << ", " << maxY << ", " << maxZ << ")" << std::endl; 
 
 	return 0;
 }
