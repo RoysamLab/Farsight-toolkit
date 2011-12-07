d260d7372897bc09a58ce7b37ca292fcc2da9ab7
Ho Cheung <hocheung20@gmail.com>
2011-12-07 20:01:15 +0000

    Fixing 8BitConverter python scripts

--- python/8BitConverter.py
+++ python/8BitConverter.py
@@ -2,10 +2,10 @@ import os, subprocess, sys
 
 if __name__ == '__main__':
 	image_list_file=open(sys.argv[1], 'r');
-	image_list = montage_tile_file.readlines()
+	image_list = image_list_file.readlines()
 
 	number_of_lines = len(image_list)
 	for x in range(number_of_lines):
-                command = 'TIF16BitTo8BitConvertor.exe ' + image_list[x].rstrip('\n')
+                command = './TIF16bitTo8bit ' + image_list[x].rstrip('\n')
                 print(command)
 		os.system(command)
