c77c0ea616d543bfa749d6386eccdf7f4c534723
Ho Cheung <hocheung20@gmail.com>
2011-12-09 18:32:44 +0000

    image_dicer now spits out txt file, 1st line is centroid in global space, 2nd line is centroid in local space

--- image_dicer/main.cpp
+++ image_dicer/main.cpp
@@ -11,8 +11,8 @@ typedef itk::Image<unsigned char, 3> ImageType;
 
 int main(int argc, char* argv[])
 {
-	std::fstream soma_centroid_file;
-	soma_centroid_file.open(argv[1], std::fstream::in);
+	std::ifstream centroids_file;
+	centroids_file.open(argv[1], std::fstream::in);
 
 	typedef itk::ImageFileReader<ImageType> ReaderType;
 	typedef itk::ImageFileWriter<ImageType> WriterType;	
@@ -29,16 +29,16 @@ int main(int argc, char* argv[])
 	ROIFilterType::Pointer ROIfilter = ROIFilterType::New();
 
 
-	while (soma_centroid_file.good())
+	while (centroids_file.good())
 	{
 		double x_d, y_d, z_d;
-		soma_centroid_file >> x_d >> y_d >> z_d;
+		centroids_file >> x_d >> y_d >> z_d;
 
 		int x, y, z;
 		x = x_d;
 		y = y_d;
 		z = z_d;
-
+		
 		//std::cout << x << " " << y << " " << z << std::endl;
 		ImageType::IndexType start;
 
@@ -51,8 +51,15 @@ int main(int argc, char* argv[])
 		size[1] = 300;
 		size[2] = 100;
 
-		std::ostringstream output_filename_stream;
+		//Open file to write the centroids out
+		std::ostringstream output_centroid_filename_stream;
+		output_centroid_filename_stream << vul_file::strip_extension(argv[2]) << "_" << x << "_" << y << "_" << z << ".txt";
+		std::ofstream centroid_file_coords(output_centroid_filename_stream.str().c_str());
 
+		centroid_file_coords << x << "_" << y << "_" << z << std::endl;	//Write global coords
+		centroid_file_coords << 150 << "_" << 150 << "_" << "50" << std::endl;	//Write lobal coords
+		centroid_file_coords.close();
+		
 		ImageType::RegionType desiredRegion;
 		desiredRegion.SetSize(size);
 		desiredRegion.SetIndex(start);
@@ -60,13 +67,14 @@ int main(int argc, char* argv[])
 		ROIfilter->SetRegionOfInterest(desiredRegion);
 		ROIfilter->SetInput(image);
 
-		output_filename_stream << vul_file::strip_extension(argv[2]) << "_" << x << "_" << y << "_" << z << ".mhd";	
+		std::ostringstream output_image_filename_stream;
+		output_image_filename_stream << vul_file::strip_extension(argv[2]) << "_" << x << "_" << y << "_" << z << ".mhd";	
 
-		writer->SetFileName(std::string(output_filename_stream.str()));
+		writer->SetFileName(std::string(output_image_filename_stream.str()));
 		writer->SetInput(ROIfilter->GetOutput());
 		writer->Update();		
 
 	}
 
-	soma_centroid_file.close();
+	centroids_file.close();
 }
