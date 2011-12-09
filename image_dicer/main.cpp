89eae29e098312c758035bcf28941a5c27f5776a
Ho Cheung <hocheung20@gmail.com>
2011-12-09 05:44:17 +0000

    image_dicer fixed so that it no longer writes the whole iamge

--- image_dicer/main.cpp
+++ image_dicer/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include "vul/vul_file.h"
 #include "iostream"
+#include "itkRegionOfInterestImageFilter.h"
 
 typedef itk::Image<unsigned char, 3> ImageType;
 
@@ -12,55 +13,61 @@ int main(int argc, char* argv[])
 {
 	std::fstream soma_centroid_file;
 	soma_centroid_file.open(argv[1], std::fstream::in);
-	
+
 	typedef itk::ImageFileReader<ImageType> ReaderType;
 	typedef itk::ImageFileWriter<ImageType> WriterType;	
-	
+
 	ReaderType::Pointer reader = ReaderType::New();
 	reader->SetFileName(argv[2]);
 	reader->Update();
-	
+
 	ImageType::Pointer image = reader->GetOutput();	
-	
+
 	WriterType::Pointer writer = WriterType::New();
 
-	
+	typedef itk::RegionOfInterestImageFilter< ImageType, ImageType > ROIFilterType;
+	ROIFilterType::Pointer ROIfilter = ROIFilterType::New();
+
+
 	while (soma_centroid_file.good())
 	{
 		double x_d, y_d, z_d;
 		soma_centroid_file >> x_d >> y_d >> z_d;
-		
+
 		int x, y, z;
 		x = x_d;
 		y = y_d;
 		z = z_d;
 
 		//std::cout << x << " " << y << " " << z << std::endl;
-		ImageType::RegionType region;
 		ImageType::IndexType start;
-	
+
 		start[0] = y - 150;
 		start[1] = x - 150;
 		start[2] = z - 50;
-		
+
 		ImageType::SizeType size;
 		size[0] = 300;
 		size[1] = 300;
 		size[2] = 100;
 
-		region.SetSize(size);
-		region.SetIndex(start);
-
 		std::ostringstream output_filename_stream;
+
+		ImageType::RegionType desiredRegion;
+		desiredRegion.SetSize(size);
+		desiredRegion.SetIndex(start);
+
+		ROIfilter->SetRegionOfInterest(desiredRegion);
+		ROIfilter->SetInput(image);
 
 		output_filename_stream << vul_file::strip_extension(argv[2]) << "_" << x << "_" << y << "_" << z << ".mhd";	
 
 		writer->SetFileName(std::string(output_filename_stream.str()));
-		writer->SetInput(image);
+		writer->SetInput(ROIfilter->GetOutput());
 		writer->Update();		
 
 	}
-	
+
 	soma_centroid_file.close();
 }
 
