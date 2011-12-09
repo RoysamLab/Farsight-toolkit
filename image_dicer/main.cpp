374d4146b4bc6331b3fbef8076b9496e5aa7de19
Ho Cheung <hocheung20@gmail.com>
2011-12-09 04:54:13 +0000

    image_dicer python script

--- image_dicer/main.cpp
+++ image_dicer/main.cpp
@@ -10,58 +10,44 @@ typedef itk::Image<unsigned char, 3> ImageType;
 
 int main(int argc, char* argv[])
 {
-	std::fstream soma_centroid_file;
-	soma_centroid_file.open(argv[1], std::fstream::in);
-	
 	typedef itk::ImageFileReader<ImageType> ReaderType;
 	typedef itk::ImageFileWriter<ImageType> WriterType;	
 	
 	ReaderType::Pointer reader = ReaderType::New();
-	reader->SetFileName(argv[2]);
+	reader->SetFileName(argv[1]);
 	//reader->Update();
 	
 	//ImageType::Pointer image = reader->GetOutput();	
 	
 	WriterType::Pointer writer = WriterType::New();
 
+	ImageType::RegionType region;
+	ImageType::IndexType start;
 	
-	while (soma_centroid_file.good())
-	{
-		double x_d, y_d, z_d;
-		soma_centroid_file >> x_d >> y_d >> z_d;
+	int x = atoi(argv[2]);
+	int y = atoi(argv[3]);
+	int z = atoi(argv[4]);
+	
+	start[0] = y - 150;
+	start[1] = x - 150;
+	start[2] = z - 50;
 		
-		int x, y, z;
-		x = x_d;
-		y = y_d;
-		z = z_d;
+	ImageType::SizeType size;
+	size[0] = 300;
+	size[1] = 300;
+	size[2] = 100;
 
-		//std::cout << x << " " << y << " " << z << std::endl;
-		ImageType::RegionType region;
-		ImageType::IndexType start;
-	
-		start[0] = y - 150;
-		start[1] = x - 150;
-		start[2] = z - 50;
-		
-		ImageType::SizeType size;
-		size[0] = 300;
-		size[1] = 300;
-		size[2] = 100;
+	region.SetSize(size);
+	region.SetIndex(start);
 
-		region.SetSize(size);
-		region.SetIndex(start);
+	std::ostringstream output_filename_stream;
 
-		std::ostringstream output_filename_stream;
+	output_filename_stream << vul_file::strip_extension(argv[2]) << "_" << x << "_" << y << "_" << z << ".TIF";	
 
-		output_filename_stream << vul_file::strip_extension(argv[2]) << "_" << x << "_" << y << "_" << z << ".TIF";	
+	writer->SetFileName(std::string(output_filename_stream.str()));
+	writer->SetInput(reader->GetOutput());
+	writer->Update();		
 
-		writer->SetFileName(std::string(output_filename_stream.str()));
-		writer->SetInput(reader->GetOutput());
-		writer->Update();		
-
-	}
-	
-	soma_centroid_file.close();
 }
 
 
