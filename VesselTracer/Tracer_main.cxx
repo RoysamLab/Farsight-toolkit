41833bb2ce14b95b5a3b94943e2a894acacc35b9
Matt McCormick <Matthew.McCormick@kitware.com>
2012-01-26 01:22:02 +0000

    error: ‘::main’ must return ‘int’

--- VesselTracer/Tracer_main.cxx
+++ VesselTracer/Tracer_main.cxx
@@ -13,18 +13,20 @@
  . Side-by-side visualization of preprocessing result
  */
 
-void main(int argc, char* argv[]){
+int main(int argc, char* argv[]){
 
-	
+
 	// IO for now..
 	std::string input_data_path = "C:\\Lab\\data\\Amit\\25_6_1099_AJ_left_b_Ch1_cropped.tif";
 	//std::string input_data_path = "C:\\Lab\\data\\forAstrocytes\\ASTRO_Cropped2_montage_8bitkt11410_w212TRITCdsu.tif";
-	
+
 	bool preprocess = false; //false;
 	bool startWithMST = false; //true; //false;
-	
+
 	ftkVesselTracer *Tracer = new ftkVesselTracer(input_data_path, preprocess, startWithMST);
-	
+
 	// Please do not use this, it is not cross platform.
 	//_getch();
+
+	return 0;
 }
