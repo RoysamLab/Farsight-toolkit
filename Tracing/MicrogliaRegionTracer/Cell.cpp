2a2696f75d1bfbfdfbd6eed46f82a562505639c4
Ho Cheung <hocheung20@gmail.com>
2012-02-02 17:51:35 +0000

    Changed some size_t to itk::uint64_t

--- Tracing/MicrogliaRegionTracer/Cell.cpp
+++ Tracing/MicrogliaRegionTracer/Cell.cpp
@@ -7,17 +7,17 @@ Cell::Cell(itk::uint64_t cell_x, itk::uint64_t cell_y, itk::uint64_t cell_z)
 	this->cell_z = cell_z;
 }
 
-size_t Cell::getX() const
+itk::uint64_t Cell::getX() const
 {
 	return cell_x;
 }
 
-size_t Cell::getY() const
+itk::uint64_t Cell::getY() const
 {
 	return cell_y;
 }
 
-size_t Cell::getZ() const
+itk::uint64_t Cell::getZ() const
 {
 	return cell_z;
 }
