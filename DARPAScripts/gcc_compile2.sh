7b624176324c69bbd8629f69d5ab302b9cc971ba
Kedar Grama <gbkedar@gmail.com>
2012-02-14 11:56:17 +0000

    Update to cloog path part 2

--- DARPAScripts/gcc_compile2.sh
+++ DARPAScripts/gcc_compile2.sh
@@ -9,10 +9,10 @@ cd .. && rm ppl-0.11.2.tar.bz2 && rm -fr ppl-0.11.2
 
 ##cloog
 wget ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.16.1.tar.gz
-tar -xvzf cloog-0.17.0.tar.gz
-cd cloog-0.17.0
+tar -xvzf cloog-0.16.1.tar.gz
+cd cloog-0.16.1
 ./configure --prefix=/data/research/gcc --with-gmp=system --with-gmp-prefix=/data/research/gcc > output.log 2>&1 && make -j75 install >> output.log 2>&1
-cd .. && rm cloog-0.17.0.tar.gz && rm -fr cloog-0.17.0
+cd .. && rm cloog-0.16.1.tar.gz && rm -fr cloog-0.16.1
 
 ##gcc
 wget http://www.netgull.com/gcc/releases/gcc-4.6.2/gcc-4.6.2.tar.bz2
