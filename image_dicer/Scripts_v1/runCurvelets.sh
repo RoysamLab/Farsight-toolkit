b8413059590380dee529ebf2953b5805466c68d7
Nicolas Rey <nicolasreyv@gmail.com>
2012-03-14 22:37:46 +0000

    New Pipeline

---
+++ image_dicer/Scripts_v1/runCurvelets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo About to run curvelets
+cd $LOCAL_DATASET_PATH_EXE
+./curvelets $GFP_LOCAL'_BS.nrrd' $LOCAL_PARAMETERS_PATH/options_curvelets
---
+++ image_dicer/Scripts_v1/superRun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This file is the batch script that will run the PIPELINE of DARPA datasets
+
+# dont forget this
+# chmod u+x ../../farsigt3_from05/src/image_dicer/*.sh
+
+./pipelineSegAndTrace_0102.sh
+# # ./pipelineSegAndTrace_0103.sh
+# # ./pipelineSegAndTrace_0113.sh #/far04
+# # ./pipelineSegAndTrace_0117.sh #/far05
+# # ./pipelineSegAndTrace_0120.sh #/far05
+# # ./pipelineSegAndTrace_0123.sh #/far05
+# ./pipelineSegAndTrace_0128.sh #/far05
+# # ./pipelineSegAndTrace_0131.sh #/far04
+# ./pipelineSegAndTrace_1118.sh
+# ./pipelineSegAndTrace_1122.sh
+# ./pipelineSegAndTrace_1230.sh
