91244b36c90365dfeba55a9485f356eebcf17f95
Nicolas Rey <nicolasreyv@gmail.com>
2012-03-15 03:49:22 +0000

    bug fix

--- image_dicer/Scripts_v1/runCurvelets.sh
+++ image_dicer/Scripts_v1/runCurvelets.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo About to run curvelets
-cd $LOCAL_DATASET_PATH_EXE
-./curvelets $GFP_LOCAL'_BS.nrrd' $LOCAL_PARAMETERS_PATH/options_curvelets
+$LOCAL_DATASET_PATH_EXE/curvelets $GFP_LOCAL'_BS.nrrd' $LOCAL_PARAMETERS_PATH/options_curvelets
