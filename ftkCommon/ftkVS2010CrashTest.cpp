20bcfa3a54e4e8e571f7d92410582539e3986751
Matt McCormick <Matthew.McCormick@kitware.com>
2012-03-30 13:28:30 +0000

    Fix ftkVS2010CrashTest execution.

--- ftkCommon/ftkVS2010CrashTest.cpp
+++ ftkCommon/ftkVS2010CrashTest.cpp
@@ -10,18 +10,21 @@ int main(int argc, char* argv[])
 	typedef itk::Image< unsigned char, 3 > ImageType;
 	typedef itk::ImageFileReader< ImageType > ReaderType;
 
-	QString filename = "E:/Farsight_Images/MicrogliaRegionTracer/GFP/montage_8bitkt06041_w311GFPdsu.tif";
-	//QString filename = argv[1];
+	QString filename = argv[1];
 
 	ReaderType::Pointer reader = ReaderType::New();
 	reader->SetFileName(filename.toStdString());
-	reader->SetNumberOfThreads(16);
+	reader->SetNumberOfThreads(4);
 	try
 	{
 		reader->Update();
+		std::cout << "Read was successful." << std::endl;
 	}
 	catch (itk::ExceptionObject &err)
 	{
 		std::cerr << "reader Exception: " << err << std::endl;
+		return 1;
 	}
+
+	return 0;
 }
