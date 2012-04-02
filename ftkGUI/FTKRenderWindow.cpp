f390c70cbfccf15450e00e301c459b423455bea4
Vinay Somasundar <vinay.somasundar@gmail.com>
2012-04-02 16:19:59 +0000

    FTKRenderWindow added to ftkGUI

---
+++ ftkGUI/FTKRenderWindow.cpp
@@ -0,0 +1,293 @@
+
+#include "FTKRenderWindow.h"
+
+FTKRenderWindow::FTKRenderWindow(QWidget *parent)
+: QMainWindow(parent)
+{
+	this->QVTK = 0;
+	table = NULL;
+	selection = NULL;
+	selectedFeature = 1;
+	selectedColorCode = 1;
+	discreteColorMap.clear();	
+	this->setupUI();
+	createDiscreteColorMap();
+}
+
+FTKRenderWindow::~FTKRenderWindow()
+{
+	if(this->QVTK)
+	{
+		delete this->QVTK;
+	}
+}
+
+void FTKRenderWindow::setupUI(void)
+{
+	//QWidget *centralWidget = new QWidget();
+
+	featureMenu = new QMenu(tr("Select Feature"));
+	connect(featureMenu, SIGNAL(triggered(QAction *)), this, SLOT(selectFeature(QAction *)));
+	menuBar()->addMenu(featureMenu);
+
+	colorMenu = new QMenu(tr("Select Color Code"));
+	connect(colorMenu, SIGNAL(triggered(QAction *)), this, SLOT(selectColorCode(QAction *)));
+	menuBar()->addMenu(colorMenu);
+	setupColorCodeMenu();
+
+	QVTK = new QVTKWidget(this);
+	Renderer = vtkSmartPointer<vtkRenderer>::New();
+	Renderer->SetBackground(0,0,0);
+	QVTK->GetRenderWindow()->AddRenderer(Renderer);
+	this->setCentralWidget(QVTK);
+
+	setWindowTitle(tr("Render Window"));
+}
+
+void FTKRenderWindow::setModels(vtkSmartPointer<vtkTable> tbl, ObjectSelection * sels)
+{
+	table = tbl;
+	if(!sels)
+		selection = new ObjectSelection();
+	else
+		selection = sels;
+	updateFeatureMenu();
+}
+
+void FTKRenderWindow::update()
+{
+	//!detects changes in the linked space 
+	this->updateFeatureMenu();
+	QWidget::update();
+}
+
+void FTKRenderWindow::closeEvent(QCloseEvent *event)
+{
+	//! Detects window closing and emits signal 
+	emit closing(this);
+	event->accept();
+}
+
+void FTKRenderWindow::selectFeature(QAction *action)
+{
+	//! Sets the feature for the color coding of the objects
+	selectedFeature = action->toolTip().toInt();
+	action->setChecked(true);
+}
+
+void FTKRenderWindow::selectColorCode(QAction *action)
+{
+	int previous_code = selectedColorCode;
+	selectedColorCode = action->toolTip().toInt();
+	//discrete color code dialog comes here
+	if(selectedColorCode == 1)
+	{
+		classColorMap.clear();
+		action->setChecked(true);
+	}
+	else if(selectedColorCode == 2)
+	{
+		int max_class = 0;
+		for(int row=0; row<(int)table->GetNumberOfRows(); ++row)
+		{
+			if(table->GetValue(row,selectedFeature+3).ToInt() > max_class)
+				max_class = table->GetValue(row, selectedFeature+3).ToInt();
+		}
+
+		QVector<QString> colors;
+		std::map< std::string, std::vector< int > >::iterator it;
+		for(it=discreteColorMap.begin() ; it != discreteColorMap.end(); it++)
+		{
+			colors.push_back(QString::fromStdString((*it).first));
+		}
+
+		DiscreteColorDialog *dialog = new DiscreteColorDialog(max_class, colors, this);
+		if( dialog->exec() )
+		{
+			classColorMap = dialog->GetClassColorMap();
+			action->setChecked(true);
+		}
+		else
+		{
+			selectedColorCode = previous_code;
+			return;
+		}
+		delete dialog;
+	}	
+}
+
+//void FTKRenderWindow::updateRenderView(void)
+//{
+//}
+
+void FTKRenderWindow::updateFeatureMenu(void)
+{
+	if(!table) return;
+
+	featureMenu->clear();
+
+	QActionGroup *featureGroup = new QActionGroup(this);
+	
+	for (int c=4; c<table->GetNumberOfColumns(); ++c)
+	{
+		QString name = QString(table->GetColumnName(c));
+		QAction *featureAct = new QAction( name, this );
+		featureAct->setToolTip( QString::number(c-3) );
+		featureAct->setCheckable(true);
+
+		featureMenu->addAction(featureAct);
+		featureGroup->addAction(featureAct);
+
+		if((c-3) == selectedFeature)
+			featureAct->setChecked(true);
+
+	}
+}
+
+void FTKRenderWindow::setupColorCodeMenu(void)
+{
+	colorMenu->clear();
+
+	QActionGroup *colorGroup = new QActionGroup(this);
+	
+	QAction *continuousColorAct = new QAction( QString("Continuous (Color changes with feature value)"), this );
+	continuousColorAct->setToolTip( QString::number(1) );
+	continuousColorAct->setCheckable(true);
+	colorMenu->addAction(continuousColorAct);
+	colorGroup->addAction(continuousColorAct);
+
+	QAction *discreteColorAct = new QAction( QString("Discrete (Choose a color for each class)"), this );
+	discreteColorAct->setToolTip( QString::number(2) );
+	discreteColorAct->setCheckable(true);
+	colorMenu->addAction(discreteColorAct);
+	colorGroup->addAction(discreteColorAct);
+
+	if(selectedColorCode == 1)
+		continuousColorAct->setChecked(true);
+	else
+		discreteColorAct->setChecked(true);
+
+}
+
+void FTKRenderWindow::createDiscreteColorMap(void)
+{
+	std::vector< int > color;
+	color.push_back(255); color.push_back(0); color.push_back(0);
+	discreteColorMap["Red"] = color;
+	color[0] = 0; color[1] = 255; color[2] = 0;
+	discreteColorMap["Green"] = color;
+	color[0] = 0; color[1] = 0; color[2] = 255;
+	discreteColorMap["Blue"] = color;
+	color[0] = 0; color[1] = 255; color[2] = 255;
+	discreteColorMap["Cyan"] = color;
+	color[0] = 255; color[1] = 165; color[2] = 0;
+	discreteColorMap["Orange"] = color;
+	color[0] = 238; color[1] = 130; color[2] = 238;
+	discreteColorMap["Violet"] = color;
+	color[0] = 255; color[1] = 255; color[2] = 0;
+	discreteColorMap["Yellow"] = color;
+	color[0] = 0; color[1] = 100; color[2] = 0;
+	discreteColorMap["Dark Green"] = color;
+	color[0] = 65; color[1] = 105; color[2] = 205;
+	discreteColorMap["Royal Blue"] = color;
+	color[0] = 190; color[1] = 190; color[2] = 190;
+	discreteColorMap["Gray"] = color;
+}
+
+
+//void FTKRenderWindow::BuildNetwork(void)
+//{
+//	std::cout << "Started building network from feature table\n";
+//
+//	data_network = new DataNetwork();
+//	for(int r=0; r<(int)table->GetNumberOfRows(); ++r)
+//	{
+//		DataNode d_node;
+//		d_node.id = table->GetValue(r,0).ToInt();
+//		d_node.x = table->GetValue(r,1).ToInt();
+//		d_node.y = table->GetValue(r,2).ToInt();
+//		d_node.z = table->GetValue(r,3).ToInt();
+//		data_network->GetDataNodesPointer().push_back(d_node);
+//	}	
+//
+//	std::cout << "Finished Building Network\n";
+//}
+
+
+//############################################################################################
+//############################################################################################
+// A DIALOG TO GET COLOR MAPPING FOR CLASSIFICATION RESULTS
+//############################################################################################
+
+DiscreteColorDialog::DiscreteColorDialog(int max_class, QVector<QString> cols, QWidget *parent)
+: QDialog(parent)
+{	
+	colors = cols;
+
+	layout = new QVBoxLayout;
+	for(int i=0; i<max_class; ++i)
+	{
+		std::stringstream ss;
+		ss << i+1;
+		QLabel * classLabel = new QLabel(QString::fromStdString("Class " + ss.str() + ":"));
+		
+		QComboBox * classCombo = new QComboBox();
+		for(int v = 0; v<colors.size(); ++v)
+		{
+			classCombo->addItem(colors[v]);
+		}
+		classCombos.push_back(classCombo);
+
+		QHBoxLayout * classLayout = new QHBoxLayout;
+		classLayout->addWidget(classLabel);
+		classLayout->addWidget(classCombo);
+
+		layout->addLayout(classLayout);
+	}
+
+	okButton = new QPushButton(tr("OK"),this);
+	connect(okButton, SIGNAL(clicked()), this, SLOT(accept()));
+	bLayout = new QHBoxLayout;
+	bLayout->addStretch(20);
+	bLayout->addWidget(okButton);
+	layout->addLayout(bLayout);
+
+	this->setLayout(layout);
+
+	this->setWindowTitle(tr("Choose Class Colors"));
+	Qt::WindowFlags flags = this->windowFlags();
+	flags &= ~Qt::WindowContextHelpButtonHint;
+	this->setWindowFlags(flags);
+}
+
+
+std::map< int, std::string > DiscreteColorDialog::GetClassColorMap(void)
+{
+	std::map< int, std::string > colorMap;
+	for(int c=0; c<classCombos.size(); ++c)
+	{
+		colorMap[c+1] = colors[classCombos[c]->currentIndex()].toStdString();
+	}
+
+	return colorMap;
+}
+
+
+//############################################################################################
+//############################################################################################
+// DATA STRUCTURES USED TO RENDER THE NETWORK
+//############################################################################################
+
+//DataNetwork::DataNetwork()
+//{
+//}
+//
+//DataNetwork::~DataNetwork()
+//{
+//	//int counter = 0;
+//	//for(int n=0; n<data_nodes.size(); n++)
+//	//	delete data_nodes[counter];
+//	//counter = 0;
+//	//for(int l=0; l<data_links.size(); l++)
+//	//	delete data_links[counter];	
+//}
---
+++ ftkGUI/FTKRenderWindow.h
@@ -0,0 +1,208 @@
+
+#ifndef RENDERWINDOW_H
+#define RENDERWINDOW_H
+
+#include <QtGui/QAction>
+#include <QtGui/QMainWindow>
+#include <QtGui/QWidget>
+#include <QtGui/QFileDialog>
+#include <QtGui/QLabel>
+#include <QtGui/QComboBox>
+#include <QtGui/QPushButton>
+#include <QtGui/QDialog>
+#include <QtGui/QStatusBar>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QCloseEvent>
+#include <QtGui/QMenuBar>
+#include <QtGui/QMenu>
+#include <QtGui/QActionGroup>
+#include <QVTKWidget.h>
+
+#include <vtkSmartPointer.h>
+#include <vtkTable.h>
+#include <vtkVariant.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyData.h>
+
+#include "ObjectSelection.h"
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <hash_map>
+
+class DataNetwork;
+struct DataNode;
+struct DataLink;
+
+#define NUM_COLORS 128
+typedef struct ren_rgb
+{
+	ren_rgb( double rv, double gv, double bv)
+	{
+		r = rv;
+		g = gv;
+		b = bv;
+	};
+	double r;
+	double g;
+	double b;
+}r_g_b;
+
+
+const static r_g_b REN_COLOR_MAP[NUM_COLORS] = {r_g_b(0.0000, 0.0000, 0.5313),r_g_b(0.0000, 0.0000, 0.5625),r_g_b(0.0000, 0.0000, 0.5938),
+r_g_b(0.0000, 0.0000, 0.6250),r_g_b(0.0000, 0.0000, 0.6563),r_g_b(0.0000, 0.0000, 0.6875),r_g_b(0.0000, 0.0000, 0.7188),
+r_g_b(0.0000, 0.0000, 0.7500),r_g_b(0.0000, 0.0000, 0.7813),r_g_b(0.0000, 0.0000, 0.8125),r_g_b(0.0000, 0.0000, 0.8438),
+r_g_b(0.0000, 0.0000, 0.8750),r_g_b(0.0000, 0.0000, 0.9063),r_g_b(0.0000, 0.0000, 0.9375),r_g_b(0.0000, 0.0000, 0.9688),
+r_g_b(0.0000, 0.0000, 1.0000),r_g_b(0.0000, 0.0313, 1.0000),r_g_b(0.0000, 0.0625, 1.0000),r_g_b(0.0000, 0.0938, 1.0000),
+r_g_b(0.0000, 0.1250, 1.0000),r_g_b(0.0000, 0.1563, 1.0000),r_g_b(0.0000, 0.1875, 1.0000),r_g_b(0.0000, 0.2188, 1.0000),
+r_g_b(0.0000, 0.2500, 1.0000),r_g_b(0.0000, 0.2813, 1.0000),r_g_b(0.0000, 0.3125, 1.0000),r_g_b(0.0000, 0.3438, 1.0000),
+r_g_b(0.0000, 0.3750, 1.0000),r_g_b(0.0000, 0.4063, 1.0000),r_g_b(0.0000, 0.4375, 1.0000),r_g_b(0.0000, 0.4688, 1.0000),
+r_g_b(0.0000, 0.5000, 1.0000),r_g_b(0.0000, 0.5313, 1.0000),r_g_b(0.0000, 0.5625, 1.0000),r_g_b(0.0000, 0.5938, 1.0000),
+r_g_b(0.0000, 0.6250, 1.0000),r_g_b(0.0000, 0.6563, 1.0000),r_g_b(0.0000, 0.6875, 1.0000),r_g_b(0.0000, 0.7188, 1.0000),
+r_g_b(0.0000, 0.7500, 1.0000),r_g_b(0.0000, 0.7813, 1.0000),r_g_b(0.0000, 0.8125, 1.0000),r_g_b(0.0000, 0.8438, 1.0000),
+r_g_b(0.0000, 0.8750, 1.0000),r_g_b(0.0000, 0.9063, 1.0000),r_g_b(0.0000, 0.9375, 1.0000),r_g_b(0.0000, 0.9688, 1.0000),
+r_g_b(0.0000, 1.0000, 1.0000),r_g_b(0.0313, 1.0000, 0.9688),r_g_b(0.0625, 1.0000, 0.9375),r_g_b(0.0938, 1.0000, 0.9063),
+r_g_b(0.1250, 1.0000, 0.8750),r_g_b(0.1563, 1.0000, 0.8438),r_g_b(0.1875, 1.0000, 0.8125),r_g_b(0.2188, 1.0000, 0.7813),
+r_g_b(0.2500, 1.0000, 0.7500),r_g_b(0.2813, 1.0000, 0.7188),r_g_b(0.3125, 1.0000, 0.6875),r_g_b(0.3438, 1.0000, 0.6563),
+r_g_b(0.3750, 1.0000, 0.6250),r_g_b(0.4063, 1.0000, 0.5938),r_g_b(0.4375, 1.0000, 0.5625),r_g_b(0.4688, 1.0000, 0.5313),
+r_g_b(0.5000, 1.0000, 0.5000),r_g_b(0.5313, 1.0000, 0.4688),r_g_b(0.5625, 1.0000, 0.4375),r_g_b(0.5938, 1.0000, 0.4063),
+r_g_b(0.6250, 1.0000, 0.3750),r_g_b(0.6563, 1.0000, 0.3438),r_g_b(0.6875, 1.0000, 0.3125),r_g_b(0.7188, 1.0000, 0.2813),
+r_g_b(0.7500, 1.0000, 0.2500),r_g_b(0.7813, 1.0000, 0.2188),r_g_b(0.8125, 1.0000, 0.1875),r_g_b(0.8438, 1.0000, 0.1563),
+r_g_b(0.8750, 1.0000, 0.1250),r_g_b(0.9063, 1.0000, 0.0938),r_g_b(0.9375, 1.0000, 0.0625),r_g_b(0.9688, 1.0000, 0.0313),
+r_g_b(1.0000, 1.0000, 0.0000),r_g_b(1.0000, 0.9688, 0.0000),r_g_b(1.0000, 0.9375, 0.0000),r_g_b(1.0000, 0.9063, 0.0000),
+r_g_b(1.0000, 0.8750, 0.0000),r_g_b(1.0000, 0.8438, 0.0000),r_g_b(1.0000, 0.8125, 0.0000),r_g_b(1.0000, 0.7813, 0.0000),
+r_g_b(1.0000, 0.7500, 0.0000),r_g_b(1.0000, 0.7188, 0.0000),r_g_b(1.0000, 0.6875, 0.0000),r_g_b(1.0000, 0.6563, 0.0000),
+r_g_b(1.0000, 0.6250, 0.0000),r_g_b(1.0000, 0.5938, 0.0000),r_g_b(1.0000, 0.5625, 0.0000),r_g_b(1.0000, 0.5313, 0.0000),
+r_g_b(1.0000, 0.5000, 0.0000),r_g_b(1.0000, 0.4688, 0.0000),r_g_b(1.0000, 0.4375, 0.0000),r_g_b(1.0000, 0.4063, 0.0000),
+r_g_b(1.0000, 0.3750, 0.0000),r_g_b(1.0000, 0.3438, 0.0000),r_g_b(1.0000, 0.3125, 0.0000),r_g_b(1.0000, 0.2813, 0.0000),
+r_g_b(1.0000, 0.2500, 0.0000),r_g_b(1.0000, 0.2188, 0.0000),r_g_b(1.0000, 0.1875, 0.0000),r_g_b(1.0000, 0.1563, 0.0000),
+r_g_b(1.0000, 0.1250, 0.0000),r_g_b(1.0000, 0.0938, 0.0000),r_g_b(1.0000, 0.0625, 0.0000),r_g_b(1.0000, 0.0313, 0.0000),
+r_g_b(1.0000, 0.0000, 0.0000),r_g_b(0.9688, 0.0000, 0.0000),r_g_b(0.9375, 0.0000, 0.0000),r_g_b(0.9063, 0.0000, 0.0000),
+r_g_b(0.8750, 0.0000, 0.0000),r_g_b(0.8438, 0.0000, 0.0000),r_g_b(0.8125, 0.0000, 0.0000),r_g_b(0.7813, 0.0000, 0.0000),
+r_g_b(0.7500, 0.0000, 0.0000),r_g_b(0.7188, 0.0000, 0.0000),r_g_b(0.6875, 0.0000, 0.0000),r_g_b(0.6563, 0.0000, 0.0000),
+r_g_b(0.6250, 0.0000, 0.0000),r_g_b(0.5938, 0.0000, 0.0000),r_g_b(0.5625, 0.0000, 0.0000),r_g_b(0.5313, 0.0000, 0.0000),
+r_g_b(0.5000, 0.0000, 0.0000)};
+
+
+class FTKRenderWindow : public QMainWindow
+{
+    Q_OBJECT;
+
+public:
+	FTKRenderWindow(QWidget *parent = 0);
+	~FTKRenderWindow();
+	void setModels(vtkSmartPointer<vtkTable> tbl, ObjectSelection * sels = NULL);
+
+public slots:
+	void update();
+
+signals:
+	void closing(QWidget *widget);
+
+protected:
+	void closeEvent(QCloseEvent *event);
+
+private slots:
+	void setupUI(void);	//for initial setup
+	void selectFeature(QAction *action);
+	void selectColorCode(QAction *action);
+	void createDiscreteColorMap(void);
+    
+private:
+	void setupColorCodeMenu();
+	void updateFeatureMenu();
+	//void BuildNetwork();
+
+	QMenu *featureMenu;
+	QMenu *colorMenu;	
+	QVTKWidget *QVTK;
+	DataNetwork *data_network;
+
+	vtkSmartPointer<vtkTable> table;
+	ObjectSelection * selection;
+	int selectedFeature;
+	int selectedColorCode;
+	std::map< int, std::string > classColorMap;
+	std::map< std::string, std::vector< int > > discreteColorMap;
+
+	vtkSmartPointer<vtkRenderer> Renderer;
+	vtkSmartPointer<vtkRenderWindowInteractor> Interactor;		
+
+};
+
+
+
+//############################################################################################
+//############################################################################################
+// A DIALOG TO GET COLOR MAPPING FOR CLASSIFICATION RESULTS
+//############################################################################################
+
+class DiscreteColorDialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+	DiscreteColorDialog(int max_class, QVector<QString> colors, QWidget *parent = 0);
+	std::map< int, std::string > GetClassColorMap(void);
+
+private:
+	QVector<QString> colors;
+	std::vector< QComboBox* > classCombos;
+	QPushButton * okButton;
+	QHBoxLayout * bLayout;
+	QVBoxLayout * layout;
+};
+
+
+//############################################################################################
+//############################################################################################
+// DATA STRUCTURES USED TO RENDER THE NETWORK
+//############################################################################################
+
+//struct DataNode
+//{
+//	int x,y,z;
+//	int time;
+//	int id;
+//	std::vector< unsigned char > color;
+//	std::vector< unsigned int > neighbors;	
+//};
+//
+//
+//struct DataLink
+//{
+//	int src_x, src_y, src_z;
+//	int trg_x, trg_y, trg_z;
+//};
+
+
+//class DataNetwork
+//{
+//public:
+//	DataNetwork();
+//	~DataNetwork();
+//
+//	//void CreatePolyDataRecursive(TraceLine* , vtkSmartPointer<vtkUnsignedCharArray> , vtkSmartPointer<vtkPoints> ,vtkSmartPointer<vtkCellArray>);
+//	vtkSmartPointer<vtkPolyData> GetVTKPolyData();
+//	//std::vector<TraceBit> CollectTraceBits();
+//	std::vector< DataNode > GetDataNodesPointer(){ return data_nodes; };
+//	//std::vector< DataLink >* GetDataLinksPointer(){ return &data_links; };
+//	stdext::hash_map<unsigned int, unsigned long long int> hash_node;
+//	stdext::hash_map<unsigned int, unsigned long long int> hash_link;
+//	vtkSmartPointer<vtkUnsignedCharArray> OriginalColors; // save the colors;
+//	
+//private:
+//
+//	std::vector<DataNode> data_nodes;	
+//	std::vector<DataLink> data_links;	
+//
+//};
+
+#endif
