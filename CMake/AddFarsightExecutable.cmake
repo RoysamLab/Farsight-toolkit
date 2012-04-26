6bd3f8433dafa9492513cb08ca96054ddfabcf4c
Zack Galbreath <zack.galbreath@gmail.com>
2012-04-26 18:45:39 +0000

    COMP: convert the rest? of the Qt apps to be Linux redistributable executables

--- CMake/AddFarsightExecutable.cmake
+++ CMake/AddFarsightExecutable.cmake
@@ -1,7 +1,16 @@
-function(add_farsight_executable exe_name srcs hdrs libs install_dir)
+# This function creates forwarding executables on Linux.  This allows us to
+# create redistrutable executables that depend on shared libraries.  At the
+# time of this writing, the primary source of shared libraries that Farsight
+# depends on is Qt.  Therefore, you should use this function to create any
+# executables that depend on Qt.
+
+function(add_farsight_executable exe_name srcs libs)
 
 if(APPLE)
   set(EXE_TYPE MACOSX_BUNDLE)
+  set(install_dir ${FARSIGHT_BUNDLE_LOCATION})
+else()
+  set(install_dir bin)
 endif()
 
 #make forwarding executable on Linux
@@ -28,7 +37,7 @@ if(NOT WIN32 AND NOT APPLE)
   add_dependencies(${exe_name} ${exe_name}${FARSIGHT_EXE_SUFFIX})
 endif(NOT WIN32 AND NOT APPLE)
 
-add_executable(${exe_name}${FARSIGHT_EXE_SUFFIX} ${EXE_TYPE} ${hdrs} ${srcs})
+add_executable(${exe_name}${FARSIGHT_EXE_SUFFIX} ${EXE_TYPE} ${srcs})
 
 target_link_libraries(${exe_name}${FARSIGHT_EXE_SUFFIX} ${libs})
 
