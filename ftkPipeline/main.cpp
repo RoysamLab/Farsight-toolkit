3ff534fb741ab583f5e78236f5833dd2f7276111
Vinay Somasundar <vinay.somasundar@gmail.com>
2012-07-29 23:46:36 +0000

    compute root features for nuclei in the pipeline

--- ftkPipeline/main.cpp
+++ ftkPipeline/main.cpp
@@ -166,6 +166,7 @@ int main(int argc, char *argv[])
 			objftkMainDarpaAstroTrace_1->runSplitting();
 			objftkMainDarpaAstroTrace_1->runInterestPoints();
 			objftkMainDarpaAstroTrace_1->runStitchRoots();
+			objftkMainDarpaAstroTrace_1->computeRootFeaturesForNuclei();
 			delete objftkMainDarpaAstroTrace_1;
 			break;
 		}
