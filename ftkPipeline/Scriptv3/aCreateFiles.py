70ee83d90d4a8becf5c2138985fdba3027e29aef
Nicolas Rey <nicolasreyv@gmail.com>
2012-08-06 21:05:16 +0000

    new create file, using different number of channels

--- ftkPipeline/Scriptv3/aCreateFiles.py
+++ ftkPipeline/Scriptv3/aCreateFiles.py
@@ -49,7 +49,7 @@ DATA_FOLDER_ALL = ['/0131_test']
 if( SERVER == 'far04' ):
 	REMOVE_MONTAGES = 1
 	#DATA_FOLDER_ALL = ['/0113_NRRD_CROPPED','/0117_NRRD_CROPPED','/0120_NRRD_CROPPED','/0123_NRRD_CROPPED','/0128_NRRD_CROPPED','/0131_NRRD_CROPPED','/0323_NRRD_CROPPED','/0405_NRRD_CROPPED','/0409_NRRD_CROPPED','/0410_NRRD_CROPPED','/0412_NRRD_CROPPED','/1206_NRRD_CROPPED']
-	#DATA_FOLDER_ALL = ['/0113_NRRD','/0117_NRRD','/0120_NRRD','/0123_NRRD','/0128_NRRD','/0131_NRRD','/0323_NRRD','/0405_NRRD','/0409_NRRD','/0410_NRRD','/0412_NRRD','/1206_NRRD']
+	DATA_FOLDER_ALL = [('/0113_NRRD',4),('/0117_NRRD',4),('/0120_NRRD',4),('/0123_NRRD',4),('/0128_NRRD',4),('/0131_NRRD',4),('/0323_NRRD',2),('/0405_NRRD',4),('/0409_NRRD',2),('/0410_NRRD',4),('/0412_NRRD',2),('/1206_NRRD',4)]
 	#DATA_FOLDER_ALL = ['/0323_NRRD','/0405_NRRD','/0409_NRRD','/0410_NRRD','/0412_NRRD','/1206_NRRD']
 	#DATA_FOLDER_ALL = ['/0113_NRRD','/0117_NRRD','/0120_NRRD','/0123_NRRD','/0128_NRRD','/0131_NRRD']
 	#DATA_FOLDER_ALL = ['/0117_NRRD','/0120_NRRD','/0123_NRRD','/0128_NRRD','/0131_NRRD']
@@ -58,13 +58,14 @@ if( SERVER == 'far04' ):
 	#DATA_FOLDER_ALL = ['/0131_test']
 	#DATA_FOLDER_ALL = ['/0131_test2']
 	#DATA_FOLDER_ALL = ['/0131_test3']
-	#DATA_FOLDER_ALL = ['/0131_test4']
+	#DATA_FOLDER_ALL = [('/0131_test4',2)]
+	#DATA_FOLDER_ALL = [('/0131_NRRD_CROPPED_3',4)]
 	#DATA_FOLDER_ALL = ['/0120_NRRD']
-	DATA_FOLDER_ALL = ['/0410_NRRD_CROPPED_2']
+	#DATA_FOLDER_ALL = ['/0410_NRRD_CROPPED_2']
 if( SERVER == 'far05' ):
 	REMOVE_MONTAGES = 1
 	#DATA_FOLDER_ALL = ['/0113_NRRD','/0117_NRRD','/0120_NRRD','/0123_NRRD','/0128_NRRD','/0131_NRRD','/0323_NRRD','/0405_NRRD','/0409_NRRD','/0410_NRRD','/0412_NRRD','/1206_NRRD']
-	DATA_FOLDER_ALL = ['/0323_NRRD','/0405_NRRD','/0409_NRRD','/0410_NRRD','/0412_NRRD','/1206_NRRD']
+	DATA_FOLDER_ALL = [('/0113_NRRD',4),('/0117_NRRD',4),('/0120_NRRD',4),('/0123_NRRD',4),('/0128_NRRD',4),('/0131_NRRD',4),('/0323_NRRD',2),('/0405_NRRD',4),('/0409_NRRD',2),('/0410_NRRD',4),('/0412_NRRD',2),('/1206_NRRD',4)]
 	#DATA_FOLDER_ALL = ['/0405_NRRD','/0409_NRRD','/0410_NRRD','/0412_NRRD','/1206_NRRD']
 	#DATA_FOLDER_ALL = ['/0131_test']
 	#DATA_FOLDER_ALL = ['/0131_test2']
@@ -72,14 +73,15 @@ if( SERVER == 'far05' ):
 	#DATA_FOLDER_ALL = ['/0131_test4']
 	runMake = 0
 if( TEST_RUN==1 ):
-	DATA_FOLDER_ALL = ['/0131_test4']
+	DATA_FOLDER_ALL = [('/0131_NRRD_CROPPED_3',4)]
 	print "THIS IS A TEST"
 	print "THIS IS A TEST"
 
 
-for DATA_FOLDER in DATA_FOLDER_ALL:
+#for DATA_FOLDER in DATA_FOLDER_ALL:
+for index, DATA_FOLDER in enumerate(DATA_FOLDER_ALL):
 
-	LOCAL_FOLDER = MAIN_DATA_FOLDER+DATA_FOLDER
+	LOCAL_FOLDER = MAIN_DATA_FOLDER+DATA_FOLDER[0]
 
 	if os.path.isdir(LOCAL_FOLDER):
 		print 'Folder '+LOCAL_FOLDER+' already exists'
@@ -102,16 +104,27 @@ for DATA_FOLDER in DATA_FOLDER_ALL:
 		BACSUBFILE.write('close();\n')
 		BACSUBFILE.close()
 
+		if DATA_FOLDER[1]==2:
+			MODEL_FILE = LOCAL_FOLDER_PARAMS+'/MG_model.txt'
+			MODEL_FILE_NAME = MODEL_FILE
+			MODEL_FILE = open(MODEL_FILE, 'w')
+			MODEL_FILE.write('bias	volume	mean	maximum	GFP_TOTAL	GFP_AVG\n')
+			MODEL_FILE.write('0	31136.5	6.70456	26.5247	224035	8.14896\n')
+			MODEL_FILE.write('0	30116.4	20.5461	40.1787	65737.4	8.02105\n')
+			MODEL_FILE.write('-0.162498	-0.0615876	1.25E-05	9.09E-06	0.331097	-1.21E-05\n')
+			MODEL_FILE.write('0.162498	0.0615875	-1.25E-05	-9.09E-06	-0.331097	1.21E-05\n')
+			MODEL_FILE.close()
 
-		MODEL_FILE = LOCAL_FOLDER_PARAMS+'/MG_model.txt'
-		MODEL_FILE_NAME = MODEL_FILE
-		MODEL_FILE = open(MODEL_FILE, 'w')
-		MODEL_FILE.write('bias	volume	mean	maximum	GFP_TOTAL	GFP_AVG\n')
-		MODEL_FILE.write('0	31136.5	6.70456	26.5247	224035	8.14896\n')
-		MODEL_FILE.write('0	30116.4	20.5461	40.1787	65737.4	8.02105\n')
-		MODEL_FILE.write('-0.162498	-0.0615876	1.25E-05	9.09E-06	0.331097	-1.21E-05\n')
-		MODEL_FILE.write('0.162498	0.0615875	-1.25E-05	-9.09E-06	-0.331097	1.21E-05\n')
-		MODEL_FILE.close()
+		if DATA_FOLDER[1]==4:
+			MODEL_FILE = LOCAL_FOLDER_PARAMS+'/MG_model.txt'
+			MODEL_FILE_NAME = MODEL_FILE
+			MODEL_FILE = open(MODEL_FILE, 'w')
+			MODEL_FILE.write('bias	volume	mean	maximum	GFP_TOTAL	GFP_AVG\n')
+			MODEL_FILE.write('0	31136.5	6.70456	26.5247	224035	8.14896\n')
+			MODEL_FILE.write('0	30116.4	20.5461	40.1787	65737.4	8.02105\n')
+			MODEL_FILE.write('-0.162498	-0.0615876	1.25E-05	9.09E-06	0.331097	-1.21E-05\n')
+			MODEL_FILE.write('0.162498	0.0615875	-1.25E-05	-9.09E-06	-0.331097	1.21E-05\n')
+			MODEL_FILE.close()
 
 		CURVE_FILE = LOCAL_FOLDER_PARAMS+'/options_curvelets'
 		CURVE_FILE = open(CURVE_FILE, 'w')
@@ -126,48 +139,103 @@ for DATA_FOLDER in DATA_FOLDER_ALL:
 		CURVE_FILE.write('-border 100\n')
 		CURVE_FILE.close()
 
-		PROJE_FILE = LOCAL_FOLDER_PARAMS+'/ProjectDefinition.xml'
-		PROJE_FILE = open(PROJE_FILE, 'w')
-		PROJE_FILE.write('<ProjectDefinition name="Project DARPA">\n')
-		PROJE_FILE.write('    <Inputs>\n')
-		PROJE_FILE.write('        <channel number="0" name="dapi" type="NUCLEAR" />\n')
-		PROJE_FILE.write('        <channel number="1" name="gfp" type="GFP_MARKER" />\n')
-		PROJE_FILE.write('    </Inputs>\n')
-		PROJE_FILE.write('    <Pipeline>\n')
-		PROJE_FILE.write('        <step name="FEATURE_COMPUTATION" />\n')
-		PROJE_FILE.write('        <step name="RAW_ASSOCIATIONS" />\n')
-		PROJE_FILE.write('        <step name="CLASSIFY_MCLR" />\n')
-		PROJE_FILE.write('    </Pipeline>\n')
-		PROJE_FILE.write('    <NuclearSegmentationParameters>\n')
-		PROJE_FILE.write('        <parameter name="high_sensitivity" value="0.00" />\n')
-		PROJE_FILE.write('        <parameter name="LoG_size" value="30.00" />\n')
-		PROJE_FILE.write('        <parameter name="min_scale" value="7.00" />\n')
-		PROJE_FILE.write('        <parameter name="max_scale" value="11.00" />\n')
-		PROJE_FILE.write('        <parameter name="xy_clustering_res" value="3.00" />\n')
-		PROJE_FILE.write('        <parameter name="z_clustering_res" value="2.00" />\n')
-		PROJE_FILE.write('        <parameter name="finalize_segmentation" value="0.00" />\n')
-		PROJE_FILE.write('        <parameter name="sampling_ratio_XY_to_Z" value="2.00" />\n')
-		PROJE_FILE.write('        <parameter name="Use_Distance_Map" value="1.00" />\n')
-		PROJE_FILE.write('        <parameter name="refinement_range" value="6.00" />\n')
-		PROJE_FILE.write('        <parameter name="min_object_size" value="100.00" />\n')
-		PROJE_FILE.write('    </NuclearSegmentationParameters>\n')
-		PROJE_FILE.write('    <CytoplasmSegmentationParameters>\n')
-		PROJE_FILE.write('        <parameter name="draw_real_boundaries" value="1.00" />\n')
-		PROJE_FILE.write('        <parameter name="remove_stromal_cell_boundaries" value="0.00" />\n')
-		PROJE_FILE.write('        <parameter name="draw_synthetic_boundaries" value="0.00" />\n')
-		PROJE_FILE.write('        <parameter name="radius_of_synthetic_boundaries" value="0.00" />\n')
-		PROJE_FILE.write('        <parameter name="number_of_levels" value="1.00" />\n')
-		PROJE_FILE.write('        <parameter name="number_of_levels_in_foreground" value="1.00" />\n')
-		PROJE_FILE.write('    </CytoplasmSegmentationParameters>\n')
-		PROJE_FILE.write('    <AssociationRules>\n')
-		PROJE_FILE.write('        <AssociationRule Name="GFP_TOTAL" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="3" Inside_Distance="8" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="TOTAL" />\n')
-		PROJE_FILE.write('        <AssociationRule Name="GFP_AVG" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="3" Inside_Distance="8" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="AVERAGE" />\n')
-		PROJE_FILE.write('    </AssociationRules>\n')
-		PROJE_FILE.write('    <Classification_MCLR_Rules>\n')
-		PROJE_FILE.write('        <Classification_MCLR_Rule ClassColName="mg" ConfThreshold="0.50" TrainingFileName="'+MODEL_FILE_NAME+'" />\n')
-		PROJE_FILE.write('    </Classification_MCLR_Rules>\n')
-		PROJE_FILE.write('</ProjectDefinition>\n')
-		PROJE_FILE.close()
+		if DATA_FOLDER[1]==2:
+			PROJE_FILE = LOCAL_FOLDER_PARAMS+'/ProjectDefinition.xml'
+			PROJE_FILE = open(PROJE_FILE, 'w')
+			PROJE_FILE.write('<ProjectDefinition name="Project DARPA">\n')
+			PROJE_FILE.write('    <Inputs>\n')
+			PROJE_FILE.write('        <channel number="0" name="dapi" type="NUCLEAR" />\n')
+			PROJE_FILE.write('        <channel number="1" name="gfp" type="GFP_MARKER" />\n')
+			PROJE_FILE.write('    </Inputs>\n')
+			PROJE_FILE.write('    <Pipeline>\n')
+			PROJE_FILE.write('        <step name="FEATURE_COMPUTATION" />\n')
+			PROJE_FILE.write('        <step name="RAW_ASSOCIATIONS" />\n')
+			PROJE_FILE.write('        <step name="CLASSIFY_MCLR" />\n')
+			PROJE_FILE.write('    </Pipeline>\n')
+			PROJE_FILE.write('    <NuclearSegmentationParameters>\n')
+			PROJE_FILE.write('        <parameter name="high_sensitivity" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="LoG_size" value="30.00" />\n')
+			PROJE_FILE.write('        <parameter name="min_scale" value="7.00" />\n')
+			PROJE_FILE.write('        <parameter name="max_scale" value="11.00" />\n')
+			PROJE_FILE.write('        <parameter name="xy_clustering_res" value="3.00" />\n')
+			PROJE_FILE.write('        <parameter name="z_clustering_res" value="2.00" />\n')
+			PROJE_FILE.write('        <parameter name="finalize_segmentation" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="sampling_ratio_XY_to_Z" value="2.00" />\n')
+			PROJE_FILE.write('        <parameter name="Use_Distance_Map" value="1.00" />\n')
+			PROJE_FILE.write('        <parameter name="refinement_range" value="6.00" />\n')
+			PROJE_FILE.write('        <parameter name="min_object_size" value="100.00" />\n')
+			PROJE_FILE.write('    </NuclearSegmentationParameters>\n')
+			PROJE_FILE.write('    <CytoplasmSegmentationParameters>\n')
+			PROJE_FILE.write('        <parameter name="draw_real_boundaries" value="1.00" />\n')
+			PROJE_FILE.write('        <parameter name="remove_stromal_cell_boundaries" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="draw_synthetic_boundaries" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="radius_of_synthetic_boundaries" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="number_of_levels" value="1.00" />\n')
+			PROJE_FILE.write('        <parameter name="number_of_levels_in_foreground" value="1.00" />\n')
+			PROJE_FILE.write('    </CytoplasmSegmentationParameters>\n')
+			PROJE_FILE.write('    <AssociationRules>\n')
+			PROJE_FILE.write('        <AssociationRule Name="GFP_TOTAL" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="3" Inside_Distance="8" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="TOTAL" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="GFP_AVG" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="3" Inside_Distance="8" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="AVERAGE" />\n')
+			PROJE_FILE.write('    </AssociationRules>\n')
+			PROJE_FILE.write('    <Classification_MCLR_Rules>\n')
+			PROJE_FILE.write('        <Classification_MCLR_Rule ClassColName="mg" ConfThreshold="0.50" TrainingFileName="'+MODEL_FILE_NAME+'" />\n')
+			PROJE_FILE.write('    </Classification_MCLR_Rules>\n')
+			PROJE_FILE.write('</ProjectDefinition>\n')
+			PROJE_FILE.close()
+
+		if DATA_FOLDER[1]==4:
+			PROJE_FILE = LOCAL_FOLDER_PARAMS+'/ProjectDefinition.xml'
+			PROJE_FILE = open(PROJE_FILE, 'w')
+			PROJE_FILE.write('<ProjectDefinition name="Project DARPA">\n')
+			PROJE_FILE.write('    <Inputs>\n')
+			PROJE_FILE.write('        <channel number="0" name="dapi" type="NUCLEAR" />\n')
+			PROJE_FILE.write('        <channel number="1" name="gfp" type="GFP_MARKER" />\n')
+			PROJE_FILE.write('        <channel number="2" name="tri" type="TRI_MARKER" />\n')
+			PROJE_FILE.write('        <channel number="3" name="cy5" type="CY5_MARKER" />\n')
+			PROJE_FILE.write('    </Inputs>\n')
+			PROJE_FILE.write('    <Pipeline>\n')
+			PROJE_FILE.write('	<step name="FEATURE_COMPUTATION" />\n')
+			PROJE_FILE.write('        <step name="RAW_ASSOCIATIONS" />\n')
+			PROJE_FILE.write('        <step name="CLASSIFY_MCLR" />\n')
+			PROJE_FILE.write('    </Pipeline>\n')
+			PROJE_FILE.write('    <NuclearSegmentationParameters>\n')
+			PROJE_FILE.write('        <parameter name="high_sensitivity" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="adaptive_binarization" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="LoG_size" value="30.00" />\n')
+			PROJE_FILE.write('        <parameter name="min_scale" value="7.00" />\n')
+			PROJE_FILE.write('        <parameter name="max_scale" value="11.00" />\n')
+			PROJE_FILE.write('        <parameter name="xy_clustering_res" value="3.00" />\n')
+			PROJE_FILE.write('        <parameter name="z_clustering_res" value="2.00" />\n')
+			PROJE_FILE.write('        <parameter name="finalize_segmentation" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="sampling_ratio_XY_to_Z" value="2.00" />\n')
+			PROJE_FILE.write('        <parameter name="Use_Distance_Map" value="1.00" />\n')
+			PROJE_FILE.write('        <parameter name="refinement_range" value="6.00" />\n')
+			PROJE_FILE.write('        <parameter name="min_object_size" value="100.00" />\n')
+			PROJE_FILE.write('    </NuclearSegmentationParameters>\n')
+			PROJE_FILE.write('    <CytoplasmSegmentationParameters>\n')
+			PROJE_FILE.write('        <parameter name="draw_real_boundaries" value="1.00" />\n')
+			PROJE_FILE.write('        <parameter name="remove_stromal_cell_boundaries" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="draw_synthetic_boundaries" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="radius_of_synthetic_boundaries" value="0.00" />\n')
+			PROJE_FILE.write('        <parameter name="number_of_levels" value="1.00" />\n')
+			PROJE_FILE.write('        <parameter name="number_of_levels_in_foreground" value="1.00" />\n')
+			PROJE_FILE.write('    </CytoplasmSegmentationParameters>\n')
+			PROJE_FILE.write('    <AssociationRules>\n')
+			PROJE_FILE.write('        <AssociationRule Name="TRI_TOTAL" SegmentationSource="NUCLEAR" Target_Image="tri" Outside_Distance="3" Inside_Distance="1" Use_Whole_Object="False" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="TOTAL" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="TRI_AVG" SegmentationSource="NUCLEAR" Target_Image="tri" Outside_Distance="3" Inside_Distance="1" Use_Whole_Object="False" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="AVERAGE" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="TRI_SURR" SegmentationSource="NUCLEAR" Target_Image="tri" Outside_Distance="3" Inside_Distance="1" Use_Whole_Object="False" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="SURROUNDEDNESS" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="GFP_TOTAL" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="0" Inside_Distance="5" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="TOTAL" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="GFP_AVG" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="0" Inside_Distance="5" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="AVERAGE" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="GFP_SURR" SegmentationSource="NUCLEAR" Target_Image="gfp" Outside_Distance="0" Inside_Distance="5" Use_Whole_Object="True" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="SURROUNDEDNESS" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="Cy5_TOTAL" SegmentationSource="NUCLEAR" Target_Image="cy5" Outside_Distance="3" Inside_Distance="0" Use_Whole_Object="False" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="TOTAL" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="Cy5_AVG" SegmentationSource="NUCLEAR" Target_Image="cy5" Outside_Distance="3" Inside_Distance="0" Use_Whole_Object="False" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="AVERAGE" />\n')
+			PROJE_FILE.write('        <AssociationRule Name="Cy5_SURR" SegmentationSource="NUCLEAR" Target_Image="cy5" Outside_Distance="3" Inside_Distance="0" Use_Whole_Object="False" Use_Background_Subtraction="True" Use_MultiLevel_Thresholding="False" Number_Of_Thresholds="1" Number_Included_In_Foreground="1" Association_Type="SURROUNDEDNESS" />\n')
+			PROJE_FILE.write('    </AssociationRules>\n')
+			PROJE_FILE.write('    <Classification_MCLR_Rules>\n')
+			PROJE_FILE.write('        <Classification_MCLR_Rule ClassColName="mg" ConfThreshold="0.50" TrainingFileName="'+MODEL_FILE_NAME+'" />\n')
+			PROJE_FILE.write('    </Classification_MCLR_Rules>\n')
+			PROJE_FILE.write('</ProjectDefinition>\n')
+
 
 		SEGME_FILE = LOCAL_FOLDER_PARAMS+'/Seg_Params.ini'
 		SEGME_FILE = open(SEGME_FILE, 'w')
