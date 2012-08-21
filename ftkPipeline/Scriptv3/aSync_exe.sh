1f638a875ab213d6aa1937f13a522464d2a82865
Nicolas Rey <nicolasreyv@gmail.com>
2012-08-21 22:29:24 +0000

    only syn exe files

---
+++ ftkPipeline/Scriptv3/aSync_exe.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp /data/nicolas/src-bin/bin/farsight-rel/exe/* far-05:/data/nicolas/src-bin/bin/farsight-rel/exe/
+#scp -r /data/nicolas/src-bin/src/farsight/trunk/ftkPipeline/* far-05:/data/nicolas/src-bin/src/farsight/trunk/ftkPipeline/
