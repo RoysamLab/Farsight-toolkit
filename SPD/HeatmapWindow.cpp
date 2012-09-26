813daac3a3d75cbf90fd1a9e4e96d0190f18dba0
Yan Xu <xy198908@gmail.com>
2012-09-26 15:15:39 +0000

    BUG: Fix lost connection signal to invoke graph window.

--- SPD/HeatmapWindow.cpp
+++ SPD/HeatmapWindow.cpp
@@ -2298,9 +2298,8 @@ void Heatmap::reselectClustersforSPD(std::set<long int>& selectedClusterSPD)
 		sampleIndex.push_back(sampleIdsVec);
 		clusIndex.push_back(clusIdsVec);
 	}
-
-	//this->Selection->SetClusterIndex(clusIndex);
-	//this->Selection->SetSampleIndex(sampleIndex);
+	this->Selection->SetClusterIndex(clusIndex);
+	this->Selection->SetSampleIndex(sampleIndex);
 }
 
 vtkSmartPointer<vtkTable> Heatmap::GetTreeTable()
