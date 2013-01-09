9267252c2dfd1934defbdfc423af245278683ad7
Ho Cheung <hocheung20@gmail.com>
2013-01-09 23:08:59 +0000

    ENH: add printing of tree size to the BoundingBoxFromTraceCalculator

--- Tracing/MicrogliaRegionTracer/BoundingBoxFromTraceCalculator.cpp
+++ Tracing/MicrogliaRegionTracer/BoundingBoxFromTraceCalculator.cpp
@@ -1,18 +1,21 @@
 #include "BoundingBoxFromTraceCalculator.h"
 #include <limits>
 #include <algorithm>
+#include <iostream>
 
 const BoundingBox BoundingBoxFromTraceCalculator::GetBoundingBoxFromTree(const Tree & tree)
 {
-    itk::uint16_t minX = std::numeric_limits<itk::uint16_t>::max();
-    itk::uint16_t maxX = std::numeric_limits<itk::uint16_t>::min();
-    itk::uint16_t minY = std::numeric_limits<itk::uint16_t>::max();
-    itk::uint16_t maxY = std::numeric_limits<itk::uint16_t>::min();
-    itk::uint16_t minZ = std::numeric_limits<itk::uint16_t>::max();
-    itk::uint16_t maxZ = std::numeric_limits<itk::uint16_t>::min();
+    itk::uint16_t minX = std::numeric_limits< itk::uint16_t >::max();
+    itk::uint16_t maxX = std::numeric_limits< itk::uint16_t >::min();
+    itk::uint16_t minY = std::numeric_limits< itk::uint16_t >::max();
+    itk::uint16_t maxY = std::numeric_limits< itk::uint16_t >::min();
+    itk::uint16_t minZ = std::numeric_limits< itk::uint16_t >::max();
+    itk::uint16_t maxZ = std::numeric_limits< itk::uint16_t >::min();
     
     NodeVectorType tree_nodes = tree.GetMemberNodes();
     
+	std::cout << "Tree has " << tree_nodes.size() << " nodes" << std::endl;
+
     NodeVectorType::iterator tree_nodes_iterator;
     
     for (tree_nodes_iterator = tree_nodes.begin(); tree_nodes_iterator != tree_nodes.end(); ++tree_nodes_iterator)
